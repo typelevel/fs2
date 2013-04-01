aabb228f7af50f179a537ceba43c16e79ab16030
Paul Chiusano <paul.chiusano@gmail.com>
2013-04-01 13:44:51 -0400

    added buffered version of yipWith

--- src/main/scala/scalaz/stream/wye.scala
+++ src/main/scala/scalaz/stream/wye.scala
@@ -1,5 +1,7 @@
 package scalaz.stream
 
+import collection.immutable.Queue
+
 import Process._
 
 trait wye {
@@ -14,6 +16,29 @@ trait wye {
 
   /** Nondeterministic version of `zip` which requests both sides in parallel. */
   def yip[I,I2]: Wye[I,I2,(I,I2)] = yipWith((_,_))
+
+  /** 
+   * Buffered version of `yipWith`. Allows up to `n` elements to enqueue on the
+   * left unanswered before requiring a response from the right. 
+   */ 
+  def byipWith[I,O,O2](n: Int)(f: (I,O) => O2): Wye[I,O,O2] = {
+    def go(buf: Queue[I]): Wye[I,O,O2] =
+      if (buf.size > n ) awaitR[O].flatMap { o => 
+        val (i,buf2) = buf.dequeue
+        emit(f(i,o)) ++ go(buf2)
+      } 
+      else if (buf.isEmpty) awaitL[I].flatMap { i => go(buf.enqueue(i)) }
+      else awaitBoth[I,O].flatMap {
+        case These.This(i) => go(buf.enqueue(i))
+        case These.That(o) => 
+          val (i, buf2) = buf.dequeue
+          emit(f(i,o)) ++ go(buf2)
+        case These.Both(inew,o) => 
+          val (i, buf2) = buf.dequeue
+          emit(f(i,o)) ++ go(buf.enqueue(inew)) 
+      }
+    go(Queue())
+  }
 }
 
 object wye extends wye
