b472005eb66ef6f747beeee51e020a17b348db47
Runar Bjarnason <runarorama@gmail.com>
2013-08-08 17:34:07 -0400

    Better actor

--- src/main/scala/scalaz/stream/actor.scala
+++ src/main/scala/scalaz/stream/actor.scala
@@ -2,6 +2,7 @@ package scalaz.stream
 
 import scalaz._
 import scalaz.concurrent.{Actor, Strategy, Task}
+import scalaz.\/._
 
 import QueueMsg._
 
@@ -9,42 +10,46 @@ import collection.immutable.Queue
 
 trait actor {
 
-  /** 
-   * Returns a `Process` stream that can be added to or halted 
+  /**
+   * Returns a `Process` stream that can be added to or halted
    * asynchronously using the returned `Actor`. The output
    * stream will terminate when the actor receives `QueueMsg.Close()`
-   * or `QueueMsg.Fail(e)`. The memory usage of the actor can 
+   * or `QueueMsg.Fail(e)`. The memory usage of the actor can
    * grow unbounded if `QueueMsg.Enqueue` messages are sent to the
    * actor faster than they are dequeued by whatever consumes the output `Process`.
-   * 
+   *
    * Use the `QueueMsg.QueueSize` message to asynchronously check the
    * queue size and throttle whatever is feeding the actor messages.
    */
   def queue[A](implicit S: Strategy): Task[(Actor[QueueMsg[A]], Process[Task, A])] = Task.delay {
-    var q = Queue[Throwable \/ A]()
+    var q = Queue[A]()
     var n = 0 // size of q
     var listeners: Queue[(Throwable \/ A) => Unit] = Queue()
+    var terminal: Option[Throwable] = None
+    def releaseTheHorde(e: Throwable) = {
+      terminal = Some(e)
+      listeners.foreach(_(left(e)))
+    }
     val a: Actor[QueueMsg[A]] = Actor.actor {
-      case Close() => 
-        q = Queue(-\/(Process.End)) 
-        n = 1
-      case Fail(e) => 
-        q = Queue(-\/(e))
-        n = 1
+      case Close() =>
+        releaseTheHorde(Process.End)
+      case Fail(e) =>
+        releaseTheHorde(e)
       case Enqueue(a) =>
-        if (listeners.isEmpty) { q = q.enqueue(\/-(a)); n += 1 }
-        else { 
-          val (cb, l2) = listeners.dequeue 
+        if (listeners.isEmpty) { q = q.enqueue(a); n += 1 }
+        else {
+          val (cb, l2) = listeners.dequeue
           listeners = l2
           cb(\/-(a))
         }
-      case Dequeue(cb) => 
-        if (q.isEmpty) listeners = listeners.enqueue(cb)
+      case Dequeue(cb) =>
+        if (q.isEmpty && terminal.isEmpty) listeners = listeners.enqueue(cb)
+        else if (terminal.isDefined) cb(left(terminal.get))
         else {
           val (a, q2) = q.dequeue
           q = q2
           n -= 1
-          cb(a)
+          cb(right(a))
         }
       case QueueSize(cb) => cb(n)
     }
@@ -53,7 +58,7 @@ trait actor {
   }
 
   /** Convert an `Actor[A]` to a `Sink[Task, A]`. */
-  def toSink[A](snk: Actor[A]): Process[Task, A => Task[Unit]] = 
+  def toSink[A](snk: Actor[A]): Process[Task, A => Task[Unit]] =
     Process.repeatWrap { Task.now { (a: A) => Task.delay { snk ! a } } }
 }
 
@@ -62,7 +67,7 @@ object actor extends actor
 trait QueueMsg[A]
 
 object QueueMsg {
-  case class Dequeue[A](callback: (Throwable \/ A) => Unit) extends QueueMsg[A] 
+  case class Dequeue[A](callback: (Throwable \/ A) => Unit) extends QueueMsg[A]
   case class Enqueue[A](a: A) extends QueueMsg[A]
   case class Fail[A](error: Throwable) extends QueueMsg[A]
   case class Close[A]() extends QueueMsg[A]
@@ -71,12 +76,12 @@ object QueueMsg {
   def enqueue[A](a: A): QueueMsg[A] =
     Enqueue(a)
 
-  def dequeue[A](cb: A => Unit, onError: Throwable => Unit = t => ()): QueueMsg[A] = 
+  def dequeue[A](cb: A => Unit, onError: Throwable => Unit = t => ()): QueueMsg[A] =
     Dequeue {
       case -\/(e) => onError(e)
       case \/-(a) => cb(a)
     }
-  
+
   def close[A]: QueueMsg[A] = Close[A]()
   def fail[A](err: Throwable): QueueMsg[A] = Fail(err)
 }
