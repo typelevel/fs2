0ff2bd65c0c0f9cc2904be9778cd68cd96b45bd0
Paul Chiusano <paul.chiusano@gmail.com>
2013-08-18 15:14:08 -0400

    Some cleanup to wye.scala, added timedQueue combinator

--- src/main/scala/scalaz/stream/wye.scala
+++ src/main/scala/scalaz/stream/wye.scala
@@ -1,47 +1,28 @@
 package scalaz.stream
 
 import collection.immutable.Queue
+import concurrent.duration._
 
 import scalaz.{\/, -\/, \/-}
 import Process._
 
 trait wye {
 
+  /** 
+   * A `Wye` which emits values from its right branch, but allows up to `n`
+   * elements from the left branch to enqueue unanswered before blocking
+   * on the right branch.
+   */
   def boundedQueue[I](n: Int): Wye[Any,I,I] = 
-    byipWithL(n)((i,i2) => i2) 
+    yipWithL(n)((i,i2) => i2) 
 
   /** 
-   * Left-biased, buffered version of `yip`. Allows up to `n` elements to enqueue on the
-   * left unanswered before requiring a response from the right. If buffer is empty,
-   * always reads from the left. 
-   */ 
-  def byipL[I,I2](n: Int): Wye[I,I2,(I,I2)] = 
-    byipWithL(n)((_,_))
-
-  /** 
-   * Left-biased, buffered version of `yipWith`. Allows up to `n` elements to enqueue on the
-   * left unanswered before requiring a response from the right. If buffer is empty,
-   * always reads from the left. 
-   */ 
-  def byipWithL[I,O,O2](n: Int)(f: (I,O) => O2): Wye[I,O,O2] = {
-    // todo: switch to vector, or track size
-    def go(buf: Queue[I]): Wye[I,O,O2] =
-      if (buf.size > n) awaitR[O].flatMap { o => 
-        val (i,buf2) = buf.dequeue
-        emit(f(i,o)) ++ go(buf2)
-      } 
-      else if (buf.isEmpty) awaitL[I].flatMap { i => go(buf.enqueue(i)) }
-      else awaitBoth[I,O].flatMap {
-        case These.This(i) => go(buf.enqueue(i))
-        case These.That(o) => 
-          val (i, buf2) = buf.dequeue
-          emit(f(i,o)) ++ go(buf2)
-        case These.Both(inew,o) => 
-          val (i, buf2) = buf.dequeue
-          emit(f(i,o)) ++ go(buf.enqueue(inew)) 
-      }
-    go(Queue())
-  }
+   * Nondeterminstic interleave of both inputs. Emits values whenever either
+   * of the inputs is available.
+   */
+  def either[I,I2]: Wye[I,I2,I \/ I2] = 
+    merge[I \/ I2].contramapL((i: I) => -\/(i)).
+                   contramapR((i2: I2) => \/-(i2))
 
   /** 
    * Nondeterminstic interleave of both inputs. Emits values whenever either
@@ -60,9 +41,47 @@ trait wye {
     go(true)
   }
 
-  def either[I,I2]: Wye[I,I2,I \/ I2] = 
-    merge[I \/ I2].contramapL((i: I) => -\/(i)).
-                   contramapR((i2: I2) => \/-(i2))
+  /** 
+   * A `Wye` which blocks on the right side when either a) the age of the oldest unanswered 
+   * element from the left size exceeds the given duration, or b) the number of unanswered 
+   * elements from the left exceeds `maxSize`.
+   */
+  def timedQueue[I](d: Duration, maxSize: Int = Int.MaxValue): Wye[Duration,I,I] = {
+    def go(q: Vector[Duration]): Wye[Duration,I,I] = 
+      awaitBoth[Duration,I].flatMap {
+        case These.This(d2) => 
+          if (q.size >= maxSize || (d2 - q.headOption.getOrElse(d2) > d))
+            awaitR[I].flatMap(i => emit(i) then go(q.drop(1)))  
+          else
+            go(q :+ d2)
+        case These.That(i) => emit(i) then (go(q.drop(1)))
+        case These.Both(t,i) => emit(i) then (go(q.drop(1) :+ t))
+      }
+    go(Vector())
+  }
+
+  /** 
+   * `Wye` which repeatedly awaits both branches, emitting any values
+   * received from the right. Useful in conjunction with `connect`,
+   * for instance `src.connect(snk)(unboundedQueue)`
+   */
+  def unboundedQueue[I]: Wye[Any,I,I] = 
+    awaitBoth[Any,I].flatMap {
+      case These.This(any) => halt
+      case These.That(i) => emit(i) then unboundedQueue
+      case These.Both(_,i) => emit(i) then unboundedQueue
+    }
+
+  /** Nondeterministic version of `zip` which requests both sides in parallel. */
+  def yip[I,I2]: Wye[I,I2,(I,I2)] = yipWith((_,_))
+
+  /** 
+   * Left-biased, buffered version of `yip`. Allows up to `n` elements to enqueue on the
+   * left unanswered before requiring a response from the right. If buffer is empty,
+   * always reads from the left. 
+   */ 
+  def yipL[I,I2](n: Int): Wye[I,I2,(I,I2)] = 
+    yipWithL(n)((_,_))
 
   /** Nondeterministic version of `zipWith` which requests both sides in parallel. */
   def yipWith[I,I2,O](f: (I,I2) => O): Wye[I,I2,O] = 
@@ -72,8 +91,24 @@ trait wye {
       case These.Both(i,i2) => emit(f(i,i2))
     }.repeat
 
-  /** Nondeterministic version of `zip` which requests both sides in parallel. */
-  def yip[I,I2]: Wye[I,I2,(I,I2)] = yipWith((_,_))
+  /** 
+   * Left-biased, buffered version of `yipWith`. Allows up to `n` elements to enqueue on the
+   * left unanswered before requiring a response from the right. If buffer is empty,
+   * always reads from the left. 
+   */ 
+  def yipWithL[I,O,O2](n: Int)(f: (I,O) => O2): Wye[I,O,O2] = {
+    def go(buf: Vector[I]): Wye[I,O,O2] =
+      if (buf.size > n) awaitR[O].flatMap { o => 
+        emit(f(buf.head,o)) ++ go(buf.tail)
+      }
+      else if (buf.isEmpty) awaitL[I].flatMap { i => go(buf :+ i) }
+      else awaitBoth[I,O].flatMap {
+        case These.This(i) => go(buf :+ i)
+        case These.That(o) => emit(f(buf.head,o)) ++ go(buf.tail)
+        case These.Both(i,o) => emit(f(buf.head,o)) ++ go(buf :+ i) 
+      }
+    go(Vector())
+  }
 }
 
 object wye extends wye
