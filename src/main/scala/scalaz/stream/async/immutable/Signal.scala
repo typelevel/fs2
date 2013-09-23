8d71d8140c902b082e07e2a8f1bc51005a6ada5c
Paul Chiusano <paul.chiusano@gmail.com>
2013-09-23 17:39:46 -0400

    Split Signal into mutable and readonly interfaces

---
+++ src/main/scala/scalaz/stream/async/immutable/Signal.scala
@@ -0,0 +1,43 @@
+package scalaz.stream.async.immutable
+
+import scalaz.\/
+import scalaz.\/._
+import scalaz.concurrent._
+import java.util.concurrent.atomic._
+
+import scalaz.stream.Process
+import scalaz.stream.Process._
+
+
+trait Signal[A] { self =>
+  /** 
+   * Returns a continuous stream, indicating whether the value has changed. 
+   * This will spike `true` once for each time the value of `Signal` was changed.
+   * It will always start with `true` when the process is run or when the `Signal` is
+   * set for the first time. 
+   */
+  def changed: Process[Task, Boolean]
+
+  /** 
+   * Returns the discrete version of this signal, updated only when `value`
+   * is changed.  Value may change several times between reads, but it is
+   * guaranteed this will always get latest known value after any change. If you want
+   * to be notified about every single change use `async.queue` for signalling.
+   * 
+   * It will emit the current value of the Signal after being run or when the signal 
+   * is set for the first time
+   */
+  def discrete: Process[Task, A]
+
+  /** 
+   * Returns the continuous version of this signal, always equal to the 
+   * current `A` inside `value`.
+   */
+  def continuous: Process[Task, A]
+
+  /** 
+   * Returns the discrete version of `changed`. Will emit `Unit` 
+   * when the `value` is changed.
+   */
+  def changes: Process[Task, Unit]
+}
