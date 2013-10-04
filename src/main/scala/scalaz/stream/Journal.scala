f4d98972efc9164cc2d316177ae7e048f913be20
Paul Chiusano <paul.chiusano@gmail.com>
2013-10-04 09:07:15 -0400

    simplified representation of journal

--- src/main/scala/scalaz/stream/Journal.scala
+++ src/main/scala/scalaz/stream/Journal.scala
@@ -17,6 +17,9 @@ case class Writer[+F[_],+S,+A](run: Process[F, A \/ S]) {
   def flatMap[F2[x]>:F[x],S2>:S,B](f: A => Writer[F2,S2,B]): Writer[F2,S2,B] = 
     Writer(run.flatMap(_.fold(f andThen (_.run), s => emit(right(s)))))
 
+  def flatMapW[F2[x]>:F[x],S2,A2>:A](f: S => Writer[F2,S2,A2]): Writer[F2,S2,A2] = 
+    Writer(run.flatMap(_.fold(a => emit(left(a)), f andThen (_.run))))
+
   def pipe[B](f: Process1[A,B]): Writer[F,S,B] = 
     Writer(run.pipe(process1.liftL(f)))
 
@@ -40,6 +43,29 @@ object Writer {
   type Process1W[+S,-I,+O] = Writer[Env[I,Any]#Is,S,O] 
   type TeeW[+S,-I,-I2,+O] = Writer[Env[I,I2]#T,S,O] 
   type WyeW[+S,-I,-I2,+O] = Writer[Env[I,I2]#Y,S,O]
+  type Journal[S,-I,+O] = WyeW[Entry[S], S, I, O]
+  // type Journal[S,-I,+O] = WyeW[Entry[S], R \/ S, I, O]
+  // case object Recover extends Entry
+
+  case class Log[F[_],S,I](
+    commit: Sink[F,S],
+    commited: Process[F,S],
+    reset: Sink[F,Unit],
+    log: Sink[F,I],
+    logged: Process[F,I])
+
+  def journal[F[_],S,I,O](
+    j: Journal[S,I,O])(log: Log[F,S,I]): Channel[F,I,O] = ???
+
+  def journal[F[_]:Nondeterminism:Catchable,S,I,O](
+    j: Journal[S,I,O])(input: Process[F,I])(l: Log[F,S,I]): Process[F,O] =
+    l.commited.wye(l.logged ++ input.observe(l.log))(j.run).flatMap {
+      case -\/(o) => Process.emit(o)
+      case \/-(e) => e match {
+        case Reset => (Process.emitSeq[F,Unit](List(())) to l.reset).drain
+        case Commit(s) => (Process.emitSeq[F,S](List(s)) to l.commit).drain
+      }
+    }
 
   def halt: Writer[Nothing,Nothing,Nothing] = 
     Writer(Process.halt)
@@ -68,90 +94,29 @@ object Writer {
   def awaitBoth[I,I2]: WyeW[Nothing,I,I2,These[I,I2]] =
     lift(Process.awaitBoth[I,I2])
 
-  /** Write a single (uncommited) value to the journal. */
-  def log[I](i: I): Writer[Nothing,Entry[Nothing,I],Nothing] = 
-    Writer(Process.emit(right(Log(i)))) 
-
-  /** Commit to a new state, clearing any uncommited values. */
-  def commit[S](s: S): Writer[Nothing,Entry[S,Nothing],Nothing] = 
-    Writer(Process.emit(right(Commit(s)))) 
+  /** Commit to a new state. */
+  def commit[S](s: S): Writer[Nothing,Entry[S],Nothing] = 
+    tell(Commit(s)) 
 
   /** Reset back to the last commited state. */
-  val reset: Writer[Nothing,Entry[Nothing,Nothing],Nothing] = 
-    Writer(Process.emit(right(Reset)))
+  val reset: Writer[Nothing,Entry[Nothing],Nothing] = 
+    tell(Reset)
 }
 
-/** 
- * A `Process` which transactionally updates a given
- * state of type `S`, recording inputs `I` in the process 
- * of performing the transaction. The most recent `commit`,
- * together with any pending messages, can be used to 
- * resume the `Journal` from an identical state, which 
- * makes this type useful as a building block for persistent,
- * distributed, or replicated streams. 
- */
-case class Journal[F[_],S,I,+O] private[stream](
-    resume: (Process[F,S], Process[F,I]) => Writer[F,Entry[S,I],O]) {
-
-  def map[O2](f: O => O2): Journal[F,S,I,O2] =
-    rewrite(_.map(f))
-
-  def rewrite[O2](
-      f: Writer[F,Entry[S,I],O] => Writer[F,Entry[S,I],O2]): Journal[F,S,I,O2] =
-    Journal((s, pending) => f(resume(s, pending)))
-
-  def pipe[O2](f: Process1[O,O2]): Journal[F,S,I,O2] = 
-    rewrite(_.pipe(f))
-
-  def |>[O2](f: Process1[O,O2]): Journal[F,S,I,O2] = 
-    rewrite(_.pipe(f))
-
-  def extend[O2>:O](f: (Process[F,S], Process[F,I]) => Writer[F,Entry[S,I],O2]): 
-      Journal[F,S,I,O2] = 
-    Journal((s, pending) => 
-      Writer(resume(s,pending).run ++ f(s, pending).run)
-    )
-
-  def ++[O2>:O](j2: Journal[F,S,I,O2]): Journal[F,S,I,O2] =
-    extend((s,i) => j2.resume(s, i))
-
-  def flatMap[O2](f: O => Writer[F,Entry[S,I],O2]): Journal[F,S,I,O2] = 
-    Journal((s, pending) => resume(s,pending).flatMap(f))
-}
 
 object Journal {
   
   val W = Writer
   val P = Process
 
-  def empty[F[_],S,I]: Journal[F,S,I,Nothing] = 
-    Journal[F,S,I,Nothing]((s,pending) => W.halt)
-
-  sealed trait Entry[+S,+A] { 
-    def map[B](f: A => B): Entry[S,B] = this match {
-      case Log(a) => Log(f(a)) 
-      case r@Reset => r
-      case c@Commit(_) => c 
-    }
-    def mapS[S2](f: S => S2): Entry[S2,A] = this match {
-      case l@Log(_) => l 
+  sealed trait Entry[+S] { 
+    def map[S2](f: S => S2): Entry[S2] = this match {
       case r@Reset => r
       case Commit(s) => Commit(f(s))
     }
   }
   
-  case class Log[A](a: A) extends Entry[Nothing,A]
-  case class Commit[S](s: S) extends Entry[S,Nothing]
-  case object Reset extends Entry[Nothing,Nothing]
+  case class Commit[S](s: S) extends Entry[S]
+  case object Reset extends Entry[Nothing]
 
-  implicit def lift[S,A,B](p: Process1[A,B]): Process1[Entry[S,A],Entry[S,B]] =
-    p match {
-      case h@Halt(_) => h
-      case Emit(h, t) => Emit(h map (Log(_)), lift(t))
-      case _ => P.await1[Entry[S,A]].flatMap {
-        case Log(a) => lift(process1.feed1(a)(p))
-        case c@Commit(_) => P.emit(c) ++ lift(p) 
-        case r@Reset => P.emit(r) ++ lift(p)
-      }
-    }
 }
