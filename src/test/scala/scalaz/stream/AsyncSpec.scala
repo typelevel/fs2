5b17e82037cee74ef78f47a99b2f543a8c165ad3
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2013-12-02 16:19:04 +0100

    Fixed actor-queue specs that now race after changing execution order of async.Queue

--- src/test/scala/scalaz/stream/AsyncSpec.scala
+++ src/test/scala/scalaz/stream/AsyncSpec.scala
@@ -1,6 +1,6 @@
 package scalaz.stream
 
-import scalaz.{Equal, Nondeterminism}
+import scalaz.{\/, Equal, Nondeterminism}
 import scalaz.syntax.equal._
 import scalaz.std.anyVal._
 import scalaz.std.list._
@@ -10,18 +10,24 @@ import org.scalacheck._
 import Prop._
 
 import scalaz.concurrent.Task
+import scala.concurrent.SyncVar
 
 object AsyncSpec extends Properties("async") {
   
   property("queue") = forAll { l: List[Int] => 
-    val (q, s) = async.queue[Int]
-    val t1 = Task { 
-      l.foreach(i => q.enqueue(i))
-      q.close
-    }
-    val t2 = s.runLog
+    val (q, s) = async.queue[Option[Int]]
 
-    Nondeterminism[Task].both(t1, t2).run._2.toList == l
+    val r = new SyncVar[Throwable\/IndexedSeq[Int]]
+    s.takeWhile(_.isDefined)
+      .collect{ case Some(x) => x}.runLog.timed(3000)
+      .runAsync(r.put)
+
+    Task {
+      l.foreach(i => q.enqueue(Some(i)))
+      q.enqueue(None)
+    }.run
+
+    r.get(3000).toSeq.flatMap(_.toList.flatten) == l
   }
 
   property("ref") = forAll { l: List[Int] => 
@@ -35,4 +41,5 @@ object AsyncSpec extends Properties("async") {
 
     Nondeterminism[Task].both(t1, t2).run._2.toList.forall(_ % 23 != 0)
   }
+
 }
