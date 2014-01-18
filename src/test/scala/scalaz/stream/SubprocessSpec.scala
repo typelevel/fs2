f5d7dd5f76e5f5df42138e697b43ab45f73ea17a
Frank S. Thomas <frank@timepit.eu>
2014-01-18 09:23:54 +0100

    Add a write-only test for Subprocess

--- src/test/scala/scalaz/stream/SubprocessSpec.scala
+++ src/test/scala/scalaz/stream/SubprocessSpec.scala
@@ -4,8 +4,14 @@ import org.scalacheck._
 import Prop._
 
 object SubprocessSpec extends Properties("Subprocess") {
-  property("echo") = secure {
+  property("read-only") = secure {
     val p = Subprocess.popen("echo", "Hello World").flatMap(_.output)
     p.runLog.run.toList == List("Hello World")
+  }
+
+  property("write-only") = secure {
+    val quit = Process("quit").toSource
+    val p = Subprocess.popen("bc").flatMap(s => quit.to(s.input))
+    p.runLog.run.toList == List(())
   }
 }
