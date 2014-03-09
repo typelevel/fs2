6cf067e02fb060bbf1d3be616f5a88d491843e78
Frank S. Thomas <frank@timepit.eu>
2014-03-09 22:54:38 +0100

    Revert "Make the hash processes vals instead of defs"
    This reverts commit 9e1dc8f8d1470e26f6dd09af9585518a4f3d3e57.
    
    This was a bad idea since the same MessageDigest could be accessed
    parallel in different threads.

--- src/main/scala/scalaz/stream/hash.scala
+++ src/main/scala/scalaz/stream/hash.scala
@@ -6,22 +6,22 @@ import Process._
 
 trait hash {
   /** Computes the MD2 hash of the input elements. */
-  val md2: Process1[Bytes, Bytes] = messageDigest("MD2")
+  def md2: Process1[Bytes, Bytes] = messageDigest("MD2")
 
   /** Computes the MD5 hash of the input elements. */
-  val md5: Process1[Bytes, Bytes] = messageDigest("MD5")
+  def md5: Process1[Bytes, Bytes] = messageDigest("MD5")
 
   /** Computes the SHA-1 hash of the input elements. */
-  val sha1: Process1[Bytes, Bytes] = messageDigest("SHA-1")
+  def sha1: Process1[Bytes, Bytes] = messageDigest("SHA-1")
 
   /** Computes the SHA-256 hash of the input elements. */
-  val sha256: Process1[Bytes, Bytes] = messageDigest("SHA-256")
+  def sha256: Process1[Bytes, Bytes] = messageDigest("SHA-256")
 
   /** Computes the SHA-384 hash of the input elements. */
-  val sha384: Process1[Bytes, Bytes] = messageDigest("SHA-384")
+  def sha384: Process1[Bytes, Bytes] = messageDigest("SHA-384")
 
   /** Computes the SHA-512 hash of the input elements. */
-  val sha512: Process1[Bytes, Bytes] = messageDigest("SHA-512")
+  def sha512: Process1[Bytes, Bytes] = messageDigest("SHA-512")
 
   private def messageDigest(algorithm: String): Process1[Bytes, Bytes] = {
     def go(digest: MessageDigest): Process1[Bytes, Bytes] =
