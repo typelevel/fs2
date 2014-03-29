50587d07a22cadd5789624fa1afe4f8bf88bc284
Frank S. Thomas <frank@timepit.eu>
2014-03-29 22:58:20 +0100

    More reordering and use === consistently

--- src/test/scala/scalaz/stream/Process1Spec.scala
+++ src/test/scala/scalaz/stream/Process1Spec.scala
@@ -6,6 +6,7 @@ import scalaz.Monoid
 import scalaz.std.anyVal._
 import scalaz.std.list._
 import scalaz.std.list.listSyntax._
+import scalaz.std.vector._
 import scalaz.std.string._
 import scalaz.syntax.equal._
 import scalaz.syntax.foldable._
@@ -17,22 +18,16 @@ import TestInstances._
 
 object Process1Spec extends Properties("process1") {
   property("all") = forAll { (p: Process0[Int], p2: Process0[String], n: Int) =>
-    val g = (x: Int) => x % 7 == 0
-    val pf : PartialFunction[Int,Int] = { case x : Int if x % 2 == 0 => x}
+    val g = (x: Int) => x % 7 === 0
+    val pf : PartialFunction[Int,Int] = { case x : Int if x % 2 === 0 => x}
 
     val sm = Monoid[String]
 
-    ("id" |: {
-      ((p |> id) === p) &&  ((id |> p) === p)
+    ("collect" |: {
+      p.collect(pf).toList === p.toList.collect(pf)
     }) &&
-    ("filter" |: {
-      p.toList.filter(g) === p.filter(g).toList
-    }) &&
-    ("take" |: {
-      p.toList.take(n) === p.take(n).toList
-    }) &&
-    ("takeWhile" |: {
-      p.toList.takeWhile(g) === p.takeWhile(g).toList
+    ("collectFirst" |: {
+      p.collectFirst(pf).toList === p.toList.collectFirst(pf).toList
     }) &&
     ("drop" |: {
       p.toList.drop(n) === p.drop(n).toList
@@ -41,7 +36,7 @@ object Process1Spec extends Properties("process1") {
       p.dropLast.toList === p.toList.dropRight(1)
     }) &&
     ("dropLastIf" |: {
-      val pred = (_: Int) % 2 == 0
+      val pred = (_: Int) % 2 === 0
       val pl = p.toList
       val n = if (pl.lastOption.map(pred).getOrElse(false)) 1 else 0
       p.dropLastIf(pred).toList === pl.dropRight(n) &&
@@ -53,29 +48,29 @@ object Process1Spec extends Properties("process1") {
     ("exists" |: {
       List(p.toList.exists(g)) === p.exists(g).toList
     }) &&
+    ("find" |: {
+      p.find(_ % 2 === 0).toList === p.toList.find(_ % 2 === 0).toList
+    }) &&
+    ("filter" |: {
+      p.toList.filter(g) === p.filter(g).toList
+    }) &&
+    ("fold" |: {
+      p.fold(0)(_ + _).toList === List(p.toList.fold(0)(_ + _))
+    }) &&
+    ("foldMap" |: {
+      p.foldMap(_.toString).toList.lastOption.toList === List(p.toList.map(_.toString).fold(sm.zero)(sm.append(_,_)))
+    }) &&
     ("forall" |: {
       List(p.toList.forall(g)) === p.forall(g).toList
     }) &&
-    ("lastOr" |: {
-      p.lastOr(42).toList === p.toList.lastOption.orElse(Some(42)).toList
-    }) &&
-    ("collect" |: {
-      p.collect(pf).toList == p.toList.collect(pf)
-    }) &&
-    ("collectFirst" |: {
-      p.collectFirst(pf).toList == p.toList.collectFirst(pf).toList
-    }) &&
-    ("find" |: {
-      p.find(_ % 2 == 0).toList == p.toList.find(_ % 2 == 0).toList
-    }) &&
-    ("fold" |: {
-      p.fold(0)(_ + _).toList == List(p.toList.fold(0)(_ + _))
-    }) &&
-    ("foldMap" |: {
-      p.foldMap(_.toString).toList.lastOption.toList == List(p.toList.map(_.toString).fold(sm.zero)(sm.append(_,_)))
+    ("id" |: {
+      ((p |> id) === p) &&  ((id |> p) === p)
     }) &&
     ("intersperse" |: {
-      p.intersperse(0).toList == p.toList.intersperse(0)
+      p.intersperse(0).toList === p.toList.intersperse(0)
+    }) &&
+    ("lastOr" |: {
+      p.lastOr(42).toList === p.toList.lastOption.orElse(Some(42)).toList
     }) &&
     ("maximum" |: {
       p.maximum.toList === p.toList.maximum.toList
@@ -100,7 +95,7 @@ object Process1Spec extends Properties("process1") {
       p2.minimumOf(_.length).toList === p2.toList.map(_.length).minimum.toList
     }) &&
     ("reduce" |: {
-      p.reduce(_ + _).toList == (if (p.toList.nonEmpty) List(p.toList.reduce(_ + _)) else List())
+      p.reduce(_ + _).toList === (if (p.toList.nonEmpty) List(p.toList.reduce(_ + _)) else List())
     }) &&
     ("scan" |: {
       p.toList.scan(0)(_ - _) ===
@@ -119,6 +114,12 @@ object Process1Spec extends Properties("process1") {
     ("sum" |: {
       p.toList.sum[Int] ===
       p.toSource.sum.runLastOr(0).timed(3000).run
+    }) &&
+    ("take" |: {
+      p.toList.take(n) === p.take(n).toList
+    }) &&
+    ("takeWhile" |: {
+      p.toList.takeWhile(g) === p.takeWhile(g).toList
     })
   }
 
@@ -128,7 +129,7 @@ object Process1Spec extends Properties("process1") {
   }
 
   property("chunk") = secure {
-    Process(0, 1, 2, 3, 4).chunk(2).toList == List(Vector(0, 1), Vector(2, 3), Vector(4))
+    Process(0, 1, 2, 3, 4).chunk(2).toList === List(Vector(0, 1), Vector(2, 3), Vector(4))
   }
 
   property("chunkBy") = secure {
@@ -138,14 +139,14 @@ object Process1Spec extends Properties("process1") {
 
   property("chunkBy2") = secure {
     val s = Process(3, 5, 4, 3, 1, 2, 6)
-    s.chunkBy2(_ < _).toList == List(Vector(3, 5), Vector(4), Vector(3), Vector(1, 2, 6)) &&
-    s.chunkBy2(_ > _).toList == List(Vector(3), Vector(5, 4, 3, 1), Vector(2), Vector(6))
+    s.chunkBy2(_ < _).toList === List(Vector(3, 5), Vector(4), Vector(3), Vector(1, 2, 6)) &&
+    s.chunkBy2(_ > _).toList === List(Vector(3), Vector(5, 4, 3, 1), Vector(2), Vector(6))
   }
 
   property("last") = secure {
     var i = 0
     Process.range(0,10).last.map(_ => i += 1).runLog.run
-    i == 1
+    i === 1
   }
 
   property("repartition") = secure {
@@ -155,8 +156,8 @@ object Process1Spec extends Properties("process1") {
       List("he", "ll", "o ", "Wo", "rl", "d") &&
     Process(1, 2, 3, 4, 5).repartition(i => Vector(i, i)).toList ==
       List(1, 3, 6, 10, 15, 15) &&
-    Process[String]().repartition(_ => Vector()).toList == List() &&
-    Process("hello").repartition(_ => Vector()).toList == List()
+    Process[String]().repartition(_ => Vector()).toList.isEmpty &&
+    Process("hello").repartition(_ => Vector()).toList.isEmpty
   }
 
   property("repartition2") = secure {
@@ -174,13 +175,13 @@ object Process1Spec extends Properties("process1") {
   }
 
   property("terminated") = secure {
-    Process(1, 2, 3).terminated.toList == List(Some(1), Some(2), Some(3), None)
+    Process(1, 2, 3).terminated.toList === List(Some(1), Some(2), Some(3), None)
   }
 
   property("window") = secure {
     def window(n: Int) = Process.range(0, 5).window(n).runLog.run.toList
-    window(1) == List(Vector(0), Vector(1), Vector(2), Vector(3), Vector(4), Vector()) &&
-    window(2) == List(Vector(0, 1), Vector(1, 2), Vector(2, 3), Vector(3, 4), Vector(4)) &&
-    window(3) == List(Vector(0, 1, 2), Vector(1, 2, 3), Vector(2, 3, 4), Vector(3, 4))
+    window(1) === List(Vector(0), Vector(1), Vector(2), Vector(3), Vector(4), Vector()) &&
+    window(2) === List(Vector(0, 1), Vector(1, 2), Vector(2, 3), Vector(3, 4), Vector(4)) &&
+    window(3) === List(Vector(0, 1, 2), Vector(1, 2, 3), Vector(2, 3, 4), Vector(3, 4))
   }
 }
