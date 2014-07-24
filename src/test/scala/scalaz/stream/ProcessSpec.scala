5be4fb36acf6add4529b93774ca001a55fc1cc00
Frank S. Thomas <frank@timepit.eu>
2014-07-24 14:02:09 +0200

    Add test for pipeW and stripO

--- src/test/scala/scalaz/stream/ProcessSpec.scala
+++ src/test/scala/scalaz/stream/ProcessSpec.scala
@@ -283,6 +283,11 @@ object ProcessSpec extends Properties("Process") {
     p.pipeO(p1).stripW.runLog.run == p.stripW.pipe(p1).runLog.run
   }
 
+  property("pipeW stripO ~= stripO pipe") = forAll { (p1: Process1[Int,Int]) =>
+    val p = logged(range(1, 11).toSource)
+    p.pipeW(p1).stripO.runLog.run == p.stripO.pipe(p1).runLog.run
+  }
+
   property("runAsync cleanup") = secure {
 
     val q = async.boundedQueue[Int]()
