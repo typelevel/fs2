6f30e83c18307c63f69dbc20e0ebc27b6c70499d
Runar Bjarnason <runarorama@gmail.com>
2015-04-08 21:51:36 -0400

    Circular buffers

--- src/main/scala/scalaz/stream/async/mutable/Queue.scala
+++ src/main/scala/scalaz/stream/async/mutable/Queue.scala
@@ -107,6 +107,10 @@ trait Queue[A] {
   private[stream] def failWithCause(c:Cause): Task[Unit]
 }
 
+private[stream] object CircularBuffer {
+  def apply[A](bound: Int)(implicit S: Strategy): Queue[A] =
+    Queue.mk(bound, (as, q) => if (as.size + q.size > bound) q.drop(as.size) else q)
+}
 
 private[stream] object Queue {
 
@@ -125,8 +129,11 @@ private[stream] object Queue {
    * @tparam A
    * @return
    */
-  def apply[A](bound: Int = 0, recover: Boolean = false)(implicit S: Strategy): Queue[A] = {
+  def apply[A](bound: Int = 0, recover: Boolean = false)(implicit S: Strategy): Queue[A] =
+    mk(bound, (_, q) => q, recover)
 
+  def mk[A](bound: Int,
+            beforeEnqueue: (Seq[A], Vector[A]) => Vector[A])(implicit S: Strategy): Queue[A] = {
     sealed trait M
     case class Enqueue(a: Seq[A], cb: Throwable \/ Unit => Unit) extends M
     case class Dequeue(ref: ConsumerRef, limit: Int, cb: Throwable \/ Seq[A] => Unit) extends M
@@ -173,9 +180,6 @@ private[stream] object Queue {
           ))
       )
     }
-
-
-
 
     // publishes single size change
     def publishSize(cb: (Throwable \/ Seq[Int]) => Unit): Unit = {
@@ -229,6 +233,8 @@ private[stream] object Queue {
     }
 
     def enqueueOne(as: Seq[A], cb: Throwable \/ Unit => Unit) = {
+      queued = beforeEnqueue(as, queued)
+      import scalaz.stream.Util._
       queued = queued fast_++ as
 
       if (consumers.size > 0 && queued.size > 0) {
