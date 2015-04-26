296a3bd58eece0100053d2ba824b3ae6b8ee4fac
Daniel Spiewak <djspiewak@gmail.com>
2015-04-25 18:58:17 -0600

    Requeue probably-lost data upon dequeue close
    Fixes #372
    
    Conflicts:
    	src/main/scala/scalaz/stream/async/mutable/Queue.scala
    	src/test/scala/scalaz/stream/QueueSpec.scala

--- src/test/scala/scalaz/stream/QueueSpec.scala
+++ src/test/scala/scalaz/stream/QueueSpec.scala
@@ -184,4 +184,30 @@ object QueueSpec extends Properties("queue") {
     collected.get(5000).nonEmpty :| "items were collected" &&
       ((collected.get getOrElse Nil) == Seq(Seq(1), Seq(1, 1), Seq(2, 2), Seq(2, 2), Seq(2, 2))) :| s"saw ${collected.get getOrElse Nil}"
   }
+
+  property("dequeue.preserve-data-in-error-cases") = forAll { xs: List[Int] =>
+    xs.nonEmpty ==> {
+      val q = async.unboundedQueue[Int]
+      val hold = new SyncVar[Unit]
+
+      val setup = Process emitAll xs to q.enqueue run
+
+      val driver = q.dequeue to (Process fail (new RuntimeException("whoops"))) onComplete (Process eval_ (Task delay { hold.put(()) }))
+
+      val safeDriver = driver onHalt {
+        case Error(_) => Process.Halt(End)
+        case rsn => Process.Halt(rsn)
+      }
+
+      val recovery = for {
+        _ <- Process eval (Task delay { hold.get })
+        i <- q.dequeue take xs.length
+      } yield i
+
+      setup.run
+
+      val results = (safeDriver merge recovery).runLog.timed(3000).run
+      (results == xs) :| s"got $results"
+    }
+  }
 }
