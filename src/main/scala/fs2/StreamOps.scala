bf2df72ba1ff7fd2353ba034b5640b5b1f5d125a
Ahmad Salim Al-Sibahi <asal@itu.dk>
2015-11-30 21:43:59 +0100

    Added StreamOps syntax for `zip` and `zipWith`

--- src/main/scala/fs2/StreamOps.scala
+++ src/main/scala/fs2/StreamOps.scala
@@ -94,4 +94,13 @@ trait StreamOps[+F[_],+A] extends Process1Ops[F,A] /* with TeeOps[F,A] with WyeO
   @deprecated("use `pipe2` or `pipe2v`, which now subsumes the functionality of `wye`", "0.9")
   def wye[F2[_],B,C](s2: Stream[F2,B])(f: (Stream[F2,A], Stream[F2,B]) => Stream[F2,C])(implicit S: Sub1[F,F2])
   : Stream[F2,C] = pipe2v(s2)(f)
+
+
+  /** Alias for `[[tee.zip]](self, s2)`. */
+  def zip[F2[_], B](s2: Stream[F2,B])(implicit S:Sub1[F,F2]): Stream[F2, (A, B)] =
+    fs2.tee.zip.apply(Sub1.substStream(self), s2)
+
+  /** Alias for `[[tee.zipWith]](f)(self, s2)`. */
+  def zipWith[F2[_], B, C](f: (A, B) => C, s2: Stream[F2,B])(implicit S:Sub1[F,F2]): Stream[F2, C] =
+    fs2.tee.zipWith(f).apply(Sub1.substStream(self), s2)
 }
