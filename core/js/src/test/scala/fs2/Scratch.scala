043d1b3f0f23cacfca8b2635f64354d75abeb1be
Michael Pilquist <mpilquist@gmail.com>
2016-07-06 16:47:19 -0400

    Example effectful stream in SJS

--- core/js/src/test/scala/fs2/Scratch.scala
+++ core/js/src/test/scala/fs2/Scratch.scala
@@ -6,6 +6,6 @@ object Scratch extends JSApp {
   def main(): Unit = {
     println(Stream(1, 2, 3).toList)
     println(Stream(1, 2, 3).toVector)
-    Stream.repeatEval(Task(scala.util.Random.nextInt(20))).filter(_ >= 10).take(10).runLog.unsafeRunAsync(println)
+    Stream.repeatEval(Task(scala.util.Random.nextInt(20))).through(pipe.prefetch).filter(_ >= 10).take(10).runLog.unsafeRunAsync(println)
   }
 }
