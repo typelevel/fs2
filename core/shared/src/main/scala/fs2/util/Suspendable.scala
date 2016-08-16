21ebe984979c72513d1d0c1c21da619f922fe606
Michael Pilquist <mpilquist@gmail.com>
2016-08-15 22:38:05 -0400

    Minor ScalaDoc changes to Suspendable

--- core/shared/src/main/scala/fs2/util/Suspendable.scala
+++ core/shared/src/main/scala/fs2/util/Suspendable.scala
@@ -4,8 +4,9 @@ package fs2.util
  * Monad which supports capturing a deferred evaluation of a by-name `F[A]`.
  *
  * Evaluation is suspended until a value is extracted, typically via the `unsafeRunAsync`
- * method on the related [[Effect]] or [[Async]] type classes. Side-effects that occur
- * while evaluating a suspension are evaluated exactly once at the time of extraction.
+ * method on the related [[Effect]] type class or via a type constructor specific extraction
+ * method (e.g., `unsafeRunSync` on `Task`). Side-effects that occur while evaluating a
+ * suspension are evaluated exactly once at the time of extraction.
  */
 trait Suspendable[F[_]] extends Monad[F] {
 
