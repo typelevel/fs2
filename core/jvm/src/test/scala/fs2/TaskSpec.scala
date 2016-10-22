ea10e9b94d8c0660eb2dcde9b0c81698203fc29d
Adam Chlupacek <adam@chlupacek.cz>
2016-10-22 23:03:10 +0100

    Fix set on Task Ref

---
+++ core/jvm/src/test/scala/fs2/TaskSpec.scala
@@ -0,0 +1,36 @@
+package fs2
+
+/**
+  * Created by adamchlupacek on 22/10/16.
+  */
+class TaskSpec extends Fs2Spec{
+
+
+  "Task" - {
+
+    "Ref" - {
+
+      "Set increments nonce" in {
+
+        Task.ref[Int].flatMap{ref =>
+          ref.setPure(1).flatMap{_ =>
+            ref.access.flatMap{case ((_, set)) =>
+              ref.setPure(2).flatMap{_ =>
+                set(Right(3))
+              }
+
+            }
+          }
+
+        }.unsafeRun() shouldBe false
+
+
+      }
+
+
+    }
+
+
+  }
+
+}
