caee91d4d1a62b71f2fea20c296c4af52a7d7fa8
Michael Pilquist <mpilquist@gmail.com>
2016-11-02 09:36:51 -0400

    Fixed order of evaluation of ap and traverse

--- core/shared/src/main/scala/fs2/util/Applicative.scala
+++ core/shared/src/main/scala/fs2/util/Applicative.scala
@@ -12,8 +12,18 @@ trait Applicative[F[_]] extends Functor[F] {
   /** Lifts a pure value in to the `F` effect. */
   def pure[A](a: A): F[A]
 
-  /** Converts an `F[A]` to an `F[B]` using an effectful function `F[A => B]`. */
+  /**
+   * Converts an `F[A]` to an `F[B]` using an effectful function `F[A => B]`.
+   * The function effect is evaluated first, followed by the `F[A]` effect.
+   */
   def ap[A,B](fa: F[A])(f: F[A => B]): F[B]
+
+  /**
+   * Returns an effect that when evaluated, first evaluates `fa` then `fb` and
+   * combines the results with `f`.
+   */
+  def map2[A,B,C](fa: F[A], fb: F[B])(f: (A, B) => C): F[C] =
+    ap(fb)(ap(fa)(pure(f.curried)))
 }
 
 object Applicative {
