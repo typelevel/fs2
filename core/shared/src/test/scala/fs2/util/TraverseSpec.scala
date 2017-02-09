0b2cff559170e75466624b9cee26ad52e1c229ec
Pavel Chlupacek <pavel.chlupacek@spinoco.com>
2017-02-09 16:18:04 +0100

    Fixed leak in Signal.discrete. Added Traverse[Seq] instance.

--- core/shared/src/test/scala/fs2/util/TraverseSpec.scala
+++ core/shared/src/test/scala/fs2/util/TraverseSpec.scala
@@ -4,11 +4,18 @@ package util
 class TraverseSpec extends Fs2Spec {
 
   "Traverse" - {
-    "evaluate effects in left-to-right order" in {
+    "evaluate effects in left-to-right order (List)" in {
       var acc = collection.mutable.ListBuffer[Int]()
       val result = Traverse[List].traverse((1 to 5).toList)(n => Task.delay(acc += n))
       result.unsafeRunSync()
       acc.toList shouldBe List(1, 2, 3, 4, 5)
+    }
+
+    "evaluate effects in left-to-right order (Seq)" in {
+      var acc = collection.mutable.ListBuffer[Int]()
+      val result = Traverse[Seq].traverse( 1 to 5 )(n => Task.delay(acc += n))
+      result.unsafeRunSync()
+      acc shouldBe Seq(1, 2, 3, 4, 5)
     }
   }
 }
