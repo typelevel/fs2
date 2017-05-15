eec60020d0720af506027b3949332aef8dffe148
Paul Chiusano <paul.chiusano@gmail.com>
2017-05-15 11:02:33 -0400

    attempt at fixing Free.viewL

--- core/shared/src/main/scala/fs2/internal/Free.scala
+++ core/shared/src/main/scala/fs2/internal/Free.scala
@@ -61,53 +61,75 @@ object Free {
     case class Done[F[_], R](r: R) extends ViewL[F,R]
     case class Failed[F[_],R](error: Throwable) extends ViewL[F,R]
 
+    class Handler[F[_],R](val get: Option[Throwable => Free[F,R]]) extends AnyVal {
+      def push(onErrInner: Throwable => Free[F,R]): Handler[F,R] = get match {
+        case None => new Handler(Some(onErrInner))
+        case Some(onErr) => new Handler(Some((e: Throwable) => OnError(Try(onErrInner(e)), onErr)))
+      }
+      def attachHandler(f: Free[F,R]): Free[F,R] = get match {
+        case None => f
+        case Some(h) => OnError(f, h)
+      }
+      def apply(err: Throwable): Free[F,R] = get match {
+        case None => Fail(err)
+        case Some(h) => try h(err) catch { case e: Throwable => Fail(e) }
+      }
+      def flatMap[R2](f: R => Free[F,R2]): Handler[F,R2] = get match {
+        case None => new Handler(None)
+        case Some(h) => new Handler(Some((e: Throwable) => h(e) flatMap f)) // todo: trampoline?
+      }
+    }
+    object Handler { def empty[F[_],R]: Handler[F,R] = new Handler(None) }
+
+    // Pure(x)
+    // Bind(Eval(e), f)
+    // OnError(Bind(Eval(e), f), h)
+
     def apply[F[_], R](free: Free[F, R]): ViewL[F, R] = {
       type FreeF[x] = Free[F,x]
       type X = Any
       @annotation.tailrec
-      def go(free: Free[F, X], k: Option[X => Free[F,R]],
-                               onErr: Option[Throwable => Free[F,R]]): ViewL[F, R] = free match {
+      def go(free: Free[F, X], k: Option[X => Free[F,R]]
+                             , onErr: Handler[F,R]
+                             , onPureErr: Handler[F,R]): ViewL[F, R] = free match {
         case Pure(x) => k match {
           case None => ViewL.Done(x.asInstanceOf[R])
-          case Some(f) => go(Try(f(x)).asInstanceOf[Free[F,X]], None, None)
+          case Some(f) => go(Try(f(x)).asInstanceOf[Free[F,X]], None, Handler.empty, Handler.empty)
         }
         case Eval(fx) => k match {
-          case None => ViewL.Bound(fx, (x: X) => Free.Pure(x.asInstanceOf[R]), onErr)
-          case Some(f) => ViewL.Bound(fx, f, onErr)
+          case None => ViewL.Bound(fx, (x: X) => Free.Pure(x.asInstanceOf[R]), onErr.get)
+          case Some(f) => ViewL.Bound(fx, f, onErr.get)
         }
-        case Fail(err) => onErr match {
-          case None => ViewL.Failed(err)
-          case Some(onErr) => go(Try(onErr(err)).asInstanceOf[Free[F,X]], None, None)
-        }
-        case OnError(fx, onErrInner) => k match {
-          case None => onErr match {
-            case None => go(fx, None, Some((e: Throwable) => Try(onErrInner(e)).asInstanceOf[Free[F,R]]))
-            case Some(onErr) => go(fx, None,
-              Some((e: Throwable) => OnError(Try(onErrInner(e)).asInstanceOf[Free[F,R]], onErr)))
-          }
-          case Some(k2) => onErr match {
-            case None => go(fx, k, Some((e: Throwable) => Try(onErrInner(e)) flatMap k2))
-            case Some(onErr) => go(fx, k,
-              Some((e: Throwable) => OnError(Try(onErrInner(e)) flatMap k2, onErr)))
-          }
+        case Fail(err) =>
+          go(onPureErr(err).asInstanceOf[Free[F,X]], None, Handler.empty, Handler.empty)
+        case OnError(fx, h) => k match {
+          case None => go(fx, None, onErr.push(h.asInstanceOf[Throwable => Free[F,R]]),
+                                    onPureErr.push(h.asInstanceOf[Throwable => Free[F,R]]))
+          // (a onError h) flatMap k2
+          case Some(k2) =>
+            val h2 = h.asInstanceOf[Throwable => Free[F,R]]
+            go(fx, k, onErr.push(h2).flatMap(k2), onPureErr.push(h2).flatMap(k2))
         }
         case b: Free.Bind[F, _, X] =>
           val fw: Free[F, Any] = b.fx.asInstanceOf[Free[F, Any]]
           val f: Any => Free[F, X] = b.f.asInstanceOf[Any => Free[F, X]]
           k match {
-            case None => onErr match {
-              case None => go(fw, Some(f.asInstanceOf[X => Free[F,R]]), onErr)
-              case Some(h) => go(fw,
-                Some((x: X) => Free.Pure(()) flatMap { _ => Try(f.asInstanceOf[X => Free[F,R]](x)) onError h }),
-                onErr)
-            }
-            case Some(g) => onErr match {
-              case None => go(fw, Some(w => Try(f(w)).flatMap(g)), onErr)
-              case Some(h) => go(fw, Some(w => Try(f(w)).flatMap(g).onError(h)), onErr)
-            }
+            case None => go(fw,
+              Some((x: X) => onErr.attachHandler(Try(f.asInstanceOf[X => Free[F,R]](x)))),
+                Handler.empty,
+                onPureErr)
+            case Some(g) =>
+              // keep onErr on the error handling stack in case there's an error in pure code
+              go(fw, Some(w => onErr.attachHandler(Try(f(w)).flatMap(g))), Handler.empty, onErr)
           }
       }
-      go(free.asInstanceOf[Free[F,X]], None, None)
+      // inline use of `Try`, avoid constructing intermediate `Fail` objects
+      // basic idea - onErr and onPureErr get pushed in event of OnError ctor
+      // in event of a Bind, we attach the current onErr to the continuation and set onErr to empty,
+      // but still keep onPureErr
+      // idea being - if we are successful, the `Bound.f` continuation will reattach error handlers
+      // so don't want to dup work
+      go(free.asInstanceOf[Free[F,X]], None, Handler.empty, Handler.empty)
     }
   }
 
