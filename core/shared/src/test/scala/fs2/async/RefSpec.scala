2ff26f9f7f750994c3a75bfcaedcb9a6decb5057
Michael Pilquist <mpilquist@gmail.com>
2017-05-23 16:49:44 -0400

    Fixed whitespace in RefSpec

--- core/shared/src/test/scala/fs2/async/RefSpec.scala
+++ core/shared/src/test/scala/fs2/async/RefSpec.scala
@@ -8,29 +8,29 @@ import scala.concurrent.duration._
 
 class RefSpec extends Fs2Spec {
 
-    "Ref" - {
+  "Ref" - {
 
-      /**
-        * Tests whether set after access causes said access's
-        * set to default to no-op as the value in ref has changed
-        */
-      "Interleaving set and access " in {
+    /**
+      * Tests whether set after access causes said access's
+      * set to default to no-op as the value in ref has changed
+      */
+    "Interleaving set and access " in {
 
-        ref[IO, Int].flatMap{ ref =>
-          ref.setAsyncPure(1).flatMap{ _ =>
-            ref.access.flatMap{ case ((_, set)) =>
-              ref.setAsyncPure(2).flatMap{ _ =>
-                IO.shift(scheduler.delayedExecutionContext(100.millis)) >> set(Right(3))
-              }
+      ref[IO, Int].flatMap{ ref =>
+        ref.setAsyncPure(1).flatMap{ _ =>
+          ref.access.flatMap{ case ((_, set)) =>
+            ref.setAsyncPure(2).flatMap{ _ =>
+              IO.shift(scheduler.delayedExecutionContext(100.millis)) >> set(Right(3))
             }
           }
-        }.unsafeToFuture.map { _ shouldBe false }
-      }
+        }
+      }.unsafeToFuture.map { _ shouldBe false }
+    }
 
-      "setSync" in {
-        ref[IO, Int].flatMap { ref =>
-          ref.setSyncPure(0) >> ref.setSync(IO(1)) >> ref.get
-        }.unsafeToFuture.map { _ shouldBe 1 }
-      }
+    "setSync" in {
+      ref[IO, Int].flatMap { ref =>
+        ref.setSyncPure(0) >> ref.setSync(IO(1)) >> ref.get
+      }.unsafeToFuture.map { _ shouldBe 1 }
     }
   }
+}
