59f70ae07e802eae767d3172391ad18c14ab166c
Michael Pilquist <mpilquist@gmail.com>
2017-06-05 14:56:08 -0400

    Implemented Pipe2.stepper

--- core/shared/src/main/scala/fs2/Pipe2.scala
+++ core/shared/src/main/scala/fs2/Pipe2.scala
@@ -1,80 +1,78 @@
 package fs2
 
-// import cats.Functor
+import fs2.internal.Free
 
 object Pipe2 {
-  // /** Creates a [[Stepper]], which allows incrementally stepping a pure `Pipe2`. */
-  // def stepper[I,I2,O](p: Pipe2[Pure,I,I2,O]): Stepper[I,I2,O] = {
-  //   type Read[+R] = Either[Option[Chunk[I]] => R, Option[Chunk[I2]] => R]
-  //   def readFunctor: Functor[Read] = new Functor[Read] {
-  //     def map[A,B](fa: Read[A])(g: A => B): Read[B] = fa match {
-  //       case Left(f) => Left(f andThen g)
-  //       case Right(f) => Right(f andThen g)
-  //     }
-  //   }
-  //   def promptsL: Stream[Read,I] =
-  //     Stream.eval[Read, Option[Chunk[I]]](Left(identity)).flatMap {
-  //       case None => Stream.empty
-  //       case Some(chunk) => Stream.chunk(chunk).append(promptsL)
-  //     }
-  //   def promptsR: Stream[Read,I2] =
-  //     Stream.eval[Read, Option[Chunk[I2]]](Right(identity)).flatMap {
-  //       case None => Stream.empty
-  //       case Some(chunk) => Stream.chunk(chunk).append(promptsR)
-  //     }
-  //
-  //   def outputs: Stream[Read,O] = covary[Read,I,I2,O](p)(promptsL, promptsR)
-  //   def stepf(s: Handle[Read,O]): Free[Read, Option[(Chunk[O],Handle[Read, O])]]
-  //   = s.buffer match {
-  //       case hd :: tl => Free.pure(Some((hd, new Handle[Read,O](tl, s.stream))))
-  //       case List() => s.stream.step.flatMap { s => Pull.output1(s) }
-  //        .close.runFoldFree(None: Option[(Chunk[O],Handle[Read, O])])(
-  //         (_,s) => Some(s))
-  //     }
-  //   def go(s: Free[Read, Option[(Chunk[O],Handle[Read, O])]]): Stepper[I,I2,O] =
-  //     Stepper.Suspend { () =>
-  //       s.unroll[Read](readFunctor, Sub1.sub1[Read]) match {
-  //         case Free.Unroll.Fail(err) => Stepper.Fail(err)
-  //         case Free.Unroll.Pure(None) => Stepper.Done
-  //         case Free.Unroll.Pure(Some((hd, tl))) => Stepper.Emits(hd, go(stepf(tl)))
-  //         case Free.Unroll.Eval(recv) => recv match {
-  //           case Left(recv) => Stepper.AwaitL(chunk => go(recv(chunk)))
-  //           case Right(recv) => Stepper.AwaitR(chunk => go(recv(chunk)))
-  //         }
-  //       }
-  //     }
-  //   go(stepf(new Handle[Read,O](List(), outputs)))
-  // }
-  //
-  // /**
-  //  * Allows stepping of a pure pipe. Each invocation of [[step]] results in
-  //  * a value of the [[Stepper.Step]] algebra, indicating that the pipe is either done, it
-  //  * failed with an exception, it emitted a chunk of output, or it is awaiting input
-  //  * from either the left or right branch.
-  //  */
-  // sealed trait Stepper[-I,-I2,+O] {
-  //   import Stepper._
-  //   @annotation.tailrec
-  //   final def step: Step[I,I2,O] = this match {
-  //     case Suspend(s) => s().step
-  //     case _ => this.asInstanceOf[Step[I,I2,O]]
-  //   }
-  // }
-  //
-  // object Stepper {
-  //   private[fs2] final case class Suspend[I,I2,O](force: () => Stepper[I,I2,O]) extends Stepper[I,I2,O]
-  //
-  //   /** Algebra describing the result of stepping a pure `Pipe2`. */
-  //   sealed trait Step[-I,-I2,+O] extends Stepper[I,I2,O]
-  //   /** Pipe indicated it is done. */
-  //   final case object Done extends Step[Any,Any,Nothing]
-  //   /** Pipe failed with the specified exception. */
-  //   final case class Fail(err: Throwable) extends Step[Any,Any,Nothing]
-  //   /** Pipe emitted a chunk of elements. */
-  //   final case class Emits[I,I2,O](chunk: Chunk[O], next: Stepper[I,I2,O]) extends Step[I,I2,O]
-  //   /** Pipe is awaiting input from the left. */
-  //   final case class AwaitL[I,I2,O](receive: Option[Chunk[I]] => Stepper[I,I2,O]) extends Step[I,I2,O]
-  //   /** Pipe is awaiting input from the right. */
-  //   final case class AwaitR[I,I2,O](receive: Option[Chunk[I2]] => Stepper[I,I2,O]) extends Step[I,I2,O]
-  // }
+  /** Creates a [[Stepper]], which allows incrementally stepping a pure `Pipe2`. */
+  def stepper[I,I2,O](p: Pipe2[Pure,I,I2,O]): Stepper[I,I2,O] = {
+    type ReadSegment[R] = Either[Option[Segment[I,Unit]] => R, Option[Segment[I2,Unit]] => R]
+    type Read[R] = Free[ReadSegment,R]
+    type UO = Option[(Segment[O,Unit],Stream[Read,O])]
+
+    def prompts[X](id: ReadSegment[Option[Segment[X,Unit]]]): Stream[Read,X] = {
+      Stream.eval[Read,Option[Segment[X,Unit]]](Free.Eval(id)).flatMap {
+        case None => Stream.empty
+        case Some(segment) => Stream.segment(segment).append(prompts(id))
+      }
+    }
+    def promptsL: Stream[Read,I] = prompts[I](Left(identity))
+    def promptsR: Stream[Read,I2] = prompts[I2](Right(identity))
+
+    def stepf(s: Stream[Read,O]): Read[UO] =
+      s.pull.uncons.flatMap {
+        case None => Pull.pure(None)
+        case Some(s) => Pull.output1(s)
+      }.stream.runLast
+
+    def go(s: Read[UO]): Stepper[I,I2,O] = Stepper.Suspend { () =>
+      s.viewL.get match {
+        case Free.Pure(None) => Stepper.Done
+        case Free.Pure(Some((hd,tl))) => Stepper.Emits(hd, go(stepf(tl)))
+        case Free.Fail(t) => Stepper.Fail(t)
+        case bound: Free.Bind[ReadSegment,_,UO] =>
+          val f = bound.asInstanceOf[Free.Bind[ReadSegment,Any,UO]].f
+          val fx = bound.fx.asInstanceOf[Free.Eval[ReadSegment,UO]].fr
+          fx match {
+            case Left(recv) =>
+              Stepper.AwaitL(segment => go(Free.Bind[ReadSegment,UO,UO](Free.Pure(recv(segment)), f)))
+            case Right(recv) =>
+              Stepper.AwaitR(segment => go(Free.Bind[ReadSegment,UO,UO](Free.Pure(recv(segment)), f)))
+          }
+        case e => sys.error("Free.ViewL structure must be Pure(a), Fail(e), or Bind(Eval(fx),k), was: " + e)
+      }
+    }
+    go(stepf(p.covary[Read].apply(promptsL, promptsR)))
+  }
+
+  /**
+   * Allows stepping of a pure pipe. Each invocation of [[step]] results in
+   * a value of the [[Stepper.Step]] algebra, indicating that the pipe is either done, it
+   * failed with an exception, it emitted a chunk of output, or it is awaiting input
+   * from either the left or right branch.
+   */
+  sealed abstract class Stepper[-I,-I2,+O] {
+    import Stepper._
+    @annotation.tailrec
+    final def step: Step[I,I2,O] = this match {
+      case Suspend(s) => s().step
+      case _ => this.asInstanceOf[Step[I,I2,O]]
+    }
+  }
+
+  object Stepper {
+    private[fs2] final case class Suspend[I,I2,O](force: () => Stepper[I,I2,O]) extends Stepper[I,I2,O]
+
+    /** Algebra describing the result of stepping a pure `Pipe2`. */
+    sealed abstract class Step[-I,-I2,+O] extends Stepper[I,I2,O]
+    /** Pipe indicated it is done. */
+    final case object Done extends Step[Any,Any,Nothing]
+    /** Pipe failed with the specified exception. */
+    final case class Fail(err: Throwable) extends Step[Any,Any,Nothing]
+    /** Pipe emitted a segment of elements. */
+    final case class Emits[I,I2,O](segment: Segment[O,Unit], next: Stepper[I,I2,O]) extends Step[I,I2,O]
+    /** Pipe is awaiting input from the left. */
+    final case class AwaitL[I,I2,O](receive: Option[Segment[I,Unit]] => Stepper[I,I2,O]) extends Step[I,I2,O]
+    /** Pipe is awaiting input from the right. */
+    final case class AwaitR[I,I2,O](receive: Option[Segment[I2,Unit]] => Stepper[I,I2,O]) extends Step[I,I2,O]
+  }
 }
