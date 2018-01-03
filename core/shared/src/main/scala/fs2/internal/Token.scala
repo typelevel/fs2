1e4c625ddc7ed45c53097b943dc5b481174307e3
Michael Pilquist <mpilquist@gmail.com>
2018-01-03 13:32:02 -0500

    Made Token serializable so an Interrupted exception carrying a token can be serialized

--- core/shared/src/main/scala/fs2/internal/Token.scala
+++ core/shared/src/main/scala/fs2/internal/Token.scala
@@ -1,6 +1,6 @@
 package fs2.internal
 
 /** Represents a unique identifier (using object equality). */
-private[fs2] final class Token {
+private[fs2] final class Token extends Serializable {
   override def toString: String = s"Token(${hashCode.toHexString})"
 }
