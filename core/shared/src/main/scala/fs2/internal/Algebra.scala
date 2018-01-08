cea0b20e8a2fc5ffbad37acc21faa7c37afd4515
Michael Pilquist <mpilquist@gmail.com>
2018-01-08 12:30:35 -0500

    Reverted unnecessary FreeC.suspend in translate

--- core/shared/src/main/scala/fs2/internal/Algebra.scala
+++ core/shared/src/main/scala/fs2/internal/Algebra.scala
@@ -360,7 +360,7 @@ private[fs2] object Algebra {
           case gs: GetScope[F, O2]   => gs.asInstanceOf[Algebra[G, O2, X]]
         }
       }
-    FreeC.suspend(fr.translate[Algebra[G, O, ?]](algFtoG))
+    fr.translate[Algebra[G, O, ?]](algFtoG)
   }
 
 }
