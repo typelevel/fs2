191559b3e847279fb56e2c8919fe10b28ce44f40
Daniel Urban <urban.dani@gmail.com>
2018-02-02 21:20:21 +0100

    fs2.async.once
    Also fix a typo in a comment.

--- core/shared/src/main/scala/fs2/async/async.scala
+++ core/shared/src/main/scala/fs2/async/async.scala
@@ -109,6 +109,33 @@ package object async {
     }
 
   /**
+    * Lazily memoize `f`. For every time the returned `F[F[A]]` is
+    * bound, the effect `f` will be performed at most once (when the
+    * inner `F[A]` is bound the first time).
+    *
+    * @see `start` for eager memoization.
+    */
+  def once[F[_], A](f: F[A])(implicit F: Effect[F], ec: ExecutionContext): F[F[A]] =
+    refOf[F, Option[Promise[F, Either[Throwable, A]]]](None).map { ref =>
+      for {
+        p <- promise[F, Either[Throwable, A]]
+        ch <- ref.modify {
+          case None =>
+            Some(p)
+          case s @ Some(_) =>
+            s
+        }
+        ma <- ch.previous match {
+          case None =>
+            f.attempt.flatMap(ma => p.complete(ma) *> F.pure(ma))
+          case Some(other) =>
+            other.get
+        }
+        a <- F.fromEither(ma)
+      } yield a
+    }
+
+  /**
     * Begins asynchronous evaluation of `f` when the returned `F[Unit]` is
     * bound. Like `start` but is more efficient.
     */
