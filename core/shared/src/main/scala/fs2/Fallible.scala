0df7a5b310a79b411de434109457e6c8205fd95e
Michael Pilquist <mpilquist@gmail.com>
2018-09-03 21:50:50 -0400

    Fixed typo in Fallible scaladoc

--- core/shared/src/main/scala/fs2/Fallible.scala
+++ core/shared/src/main/scala/fs2/Fallible.scala
@@ -3,7 +3,7 @@ package fs2
 /**
   * Indicates that a stream evaluates no effects but unlike [[Pure]], may raise errors.
   *
-  * Uninhavited.
+  * Uninhabited.
   *
   * A `Stream[Fallible,O]` can be safely converted to a `Stream[F,O]` for all `F` via `s.lift[F]`,
   * provided an `ApplicativeError[F, Throwable]` is available.
