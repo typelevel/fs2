37f37a4e61f3ffa41aee46eb3fe99db498eb2993
amarrella <zioale91@gmail.com>
2018-11-05 20:59:33 +0000

    scheduler done

--- scalafix-outputs/src/main/scala/fix/Scheduler.scala
+++ scalafix-outputs/src/main/scala/fix/Scheduler.scala
@@ -2,8 +2,8 @@ package fix
 
 import cats.effect._
 import fs2._
-
 import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 abstract class SchedulerTimer[F[_]: Effect] {
 
@@ -15,6 +15,6 @@ abstract class SchedulerTimer[F[_]: Effect] {
   Stream.awakeEvery[F](duration)
   Stream.retry(Effect[F].unit, duration, _ => duration, 1)
   Stream.eval(Effect[F].unit).debounce(duration)
-  Concurrent[F].race(Effect[F].unit, s.sleep(duration))
+  Concurrent[F].race(Effect[F].unit, scheduler.sleep(duration))
   Stream.eval(Effect[F].unit).delayBy(duration)
 }
--- scalafix/src/main/scala/fix/SchedulerRules.scala
+++ scalafix/src/main/scala/fix/SchedulerRules.scala
@@ -6,76 +6,79 @@ object SchedulerRules {
 
   def apply(t: Tree)(implicit doc: SemanticDocument): List[Patch] =
     t.collect {
-      case t @ scheduler(_: Type.Name) =>
+      case t @ schedulerMatcher(_: Type.Name) =>
         Patch.replaceTree(t, timer(Type.Name("F")).toString()) //TODO: Use actual effect
-      case Term.ApplyType(scheduler(_), List(f)) =>
+      case Term.ApplyType(s, List(f)) if isScheduler(s) =>
         Patch.replaceTree(t, timer(f).toString())
       case sched @ Term.Apply(
             Term.ApplyType(Term.Select(Term.Select(s, Term.Name("effect")), Term.Name("sleep")), _),
-            List(d)) => //TODO: use symbol matcher to make sure we are changing fs2 scheduler
+            List(d)) if isScheduler(s) =>
         val timerSleep = Term.Apply(Term.Select(s, Term.Name("sleep")), List(d))
         Patch.replaceTree(sched, timerSleep.toString())
       case sched @ Term
-            .Apply(Term.ApplyType(Term.Select(s, Term.Name("sleep")), List(f)), List(d)) =>
+            .Apply(Term.ApplyType(Term.Select(s, Term.Name("sleep")), List(f)), List(d))
+          if isScheduler(s) =>
         val stream =
           Term.Apply(Term.ApplyType(Term.Select(Term.Name("Stream"), Term.Name("sleep")), List(f)),
                      List(d))
         Patch.replaceTree(sched, stream.toString())
       case sched @ Term
-            .Apply(Term.ApplyType(Term.Select(s, Term.Name("sleep_")), List(f)), List(d)) =>
+            .Apply(Term.ApplyType(Term.Select(s, Term.Name("sleep_")), List(f)), List(d))
+          if isScheduler(s) =>
         val stream =
           Term.Apply(Term.ApplyType(Term.Select(Term.Name("Stream"), Term.Name("sleep_")), List(f)),
                      List(d))
         Patch.replaceTree(sched, stream.toString())
       case sched @ Term
-            .Apply(Term.ApplyType(Term.Select(s, Term.Name("awakeEvery")), List(f)), List(d)) =>
+            .Apply(Term.ApplyType(Term.Select(s, Term.Name("awakeEvery")), List(f)), List(d))
+          if isScheduler(s) =>
         val stream = Term.Apply(
           Term.ApplyType(Term.Select(Term.Name("Stream"), Term.Name("awakeEvery")), List(f)),
           List(d))
         Patch.replaceTree(sched, stream.toString())
       case sched @ Term
-            .Apply(Term.Select(s, Term.Name("retry")), params) =>
+            .Apply(Term.Select(s, Term.Name("retry")), params) if isScheduler(s) =>
         val stream = Term.Apply(Term.Select(Term.Name("Stream"), Term.Name("retry")), params)
         Patch.replaceTree(sched, stream.toString())
       case sched @ Term.Apply(
             Term.Select(
-              Term.Apply(
-                Term.Select(Term.Name("Stream"), Term.Name("eval")),
-                List(
-                  Term.Select(
-                    Term.ApplyType(Term.Name("Effect"), List(Type.Name("F"))),
-                    Term.Name("unit")
-                  )
-                )
-              ),
+              s,
               Term.Name("through")
             ),
             List(
               Term.Apply(
-                Term.Select(Term.Name("scheduler"), Term.Name("debounce")),
-                List(Term.Name("duration"))
+                Term.Select(_, debounce @ Term.Name("debounce")),
+                d
               )
             )
-          ) =>
-        Patch.empty //TODO
-    }
-
-  def stream(f: Type, a: Term): Term =
-    f match {
-      case Type.Name("Pure") =>
-        Term.Apply(Term.Select(Term.Name("Stream"), Term.Name("emit")), List(a))
-      case _ =>
-        Term.Apply(
-          Term.Select(Term.Name("Stream"), Term.Name("eval")),
-          List(
-            Term.Apply(
-              Term.Select(Term.ApplyType(Term.Name("Applicative"), List(f)), Term.Name("pure")),
-              List(a)))
+          ) if isStream(s) =>
+        val newStream = Term.Apply(Term.Select(s, debounce), d)
+        Patch.replaceTree(sched, newStream.toString())
+      case sched @ Term.Apply(
+            Term.Select(Term.Select(s, Term.Name("effect")), Term.Name("delayCancellable")),
+            List(fa, d)) if isScheduler(s) =>
+        val concurrent = Term.Apply(
+          Term.Select(
+            Term.ApplyType(Term.Name("Concurrent"), List(Type.Name("F"))), //TODO: Use actual effect
+            Term.Name("race")),
+          List(fa, Term.Apply(Term.Select(s, Term.Name("sleep")), List(d)))
         )
+        Patch.replaceTree(sched, concurrent.toString())
+      case sched @ Term.Apply(Term.Select(s, Term.Name("delay")), List(stream, d))
+          if isScheduler(s) && isStream(stream) =>
+        val newStream = Term.Apply(Term.Select(stream, Term.Name("delayBy")), List(d))
+        Patch.replaceTree(sched, newStream.toString())
     }
 
   def timer(f: Type) = Type.Apply(Type.Name("Timer"), List(f))
 
-  val scheduler = SymbolMatcher.normalized("fs2/Scheduler")
+  def isScheduler(s: Tree)(implicit doc: SemanticDocument): Boolean =
+    getTypeSymbol(s.symbol).fold(false)(schedulerMatcher.matches)
+
+  def isStream(s: Tree)(implicit doc: SemanticDocument): Boolean =
+    getTypeSymbol(s.symbol).fold(false)(streamMatcher.matches)
+
+  val schedulerMatcher = SymbolMatcher.normalized("fs2/Scheduler")
+  val streamMatcher = SymbolMatcher.normalized("fs2/Stream")
 
 }
