22b7590ca35813630266488e56cf72b4578901dd
Michael Pilquist <mpilquist@gmail.com>
2019-02-11 10:08:22 -0500

    Updated TaglessSpec to use ChunkGen

--- core/shared/src/test/scala/fs2/TaglessSpec.scala
+++ core/shared/src/test/scala/fs2/TaglessSpec.scala
@@ -1,27 +1,13 @@
 package fs2.tagless
 
-import fs2.Chunk
+import fs2.{Chunk, ChunkGen}
 
 import cats.effect.IO
 
-import org.scalacheck._
 import org.scalatest.{FreeSpec, Matchers}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 
-class TaglessSpec extends FreeSpec with Matchers with ScalaCheckDrivenPropertyChecks {
-
-  implicit def arbChunk[A](implicit A: Arbitrary[A]): Arbitrary[Chunk[A]] = Arbitrary(
-    Gen.frequency(
-      10 -> Gen.listOf(A.arbitrary).map(as => Chunk.vector(as.toVector)),
-      10 -> Gen.listOf(A.arbitrary).map(Chunk.seq),
-      5 -> A.arbitrary.map(a => Chunk.singleton(a)),
-      1 -> Chunk.empty[A]
-    )
-  )
-
-  implicit def cogenChunk[A: Cogen]: Cogen[Chunk[A]] =
-    Cogen[List[A]].contramap(_.toList)
-
+class TaglessSpec extends FreeSpec with Matchers with ScalaCheckDrivenPropertyChecks with ChunkGen {
 
   "Stream" - {
     "chunk" in { forAll { (c: Chunk[Int]) => Stream.chunk(c).toChunk shouldBe c } }
