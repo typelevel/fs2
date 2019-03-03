e5f2eb4539599fb605648066d3d73a97bd01a88e
Michael Pilquist <mpilquist@gmail.com>
2019-03-03 18:40:18 -0500

    Added compress and hash objects

---
+++ fs3/jvm/src/main/scala/fs2/hash.scala
@@ -0,0 +1,47 @@
+package fs2
+
+import java.security.MessageDigest
+
+/** Provides various cryptographic hashes as pipes. */
+object hash {
+
+  /** Computes an MD2 digest. */
+  def md2[F[_]]: Pipe[F, Byte, Byte] = digest(MessageDigest.getInstance("MD2"))
+
+  /** Computes an MD5 digest. */
+  def md5[F[_]]: Pipe[F, Byte, Byte] = digest(MessageDigest.getInstance("MD5"))
+
+  /** Computes a SHA-1 digest. */
+  def sha1[F[_]]: Pipe[F, Byte, Byte] =
+    digest(MessageDigest.getInstance("SHA-1"))
+
+  /** Computes a SHA-256 digest. */
+  def sha256[F[_]]: Pipe[F, Byte, Byte] =
+    digest(MessageDigest.getInstance("SHA-256"))
+
+  /** Computes a SHA-384 digest. */
+  def sha384[F[_]]: Pipe[F, Byte, Byte] =
+    digest(MessageDigest.getInstance("SHA-384"))
+
+  /** Computes a SHA-512 digest. */
+  def sha512[F[_]]: Pipe[F, Byte, Byte] =
+    digest(MessageDigest.getInstance("SHA-512"))
+
+  /**
+    * Computes the digest of the source stream, emitting the digest as a chunk
+    * after completion of the source stream.
+    */
+  def digest[F[_]](digest: => MessageDigest): Pipe[F, Byte, Byte] =
+    in =>
+      Stream.suspend {
+        in.chunks
+          .fold(digest) { (d, c) =>
+            val bytes = c.toBytes
+            d.update(bytes.values, bytes.offset, bytes.size)
+            d
+          }
+          .flatMap { d =>
+            Stream.chunk(Chunk.bytes(d.digest()))
+          }
+    }
+}
--- fs3/shared/src/test/scala/fs2/MiscellaneousGenerators.scala
+++ fs3/shared/src/test/scala/fs2/MiscellaneousGenerators.scala
@@ -7,4 +7,7 @@ trait MiscellaneousGenerators {
 
   implicit val throwableGenerator: Generator[Throwable] =
     specificValue(new Err)
+
+  implicit def arrayGenerator[A: Generator: reflect.ClassTag]: Generator[Array[A]] =
+    lists[A].map(_.toArray)
 }
