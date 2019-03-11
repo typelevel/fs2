6ed3c1ddf05fe78ae519c824ec824bc1711d8d8d
Michael Pilquist <mpilquist@gmail.com>
2019-03-11 15:13:45 -0400

    Ported MergeJoinSpec

--- core/shared/src/test/scala/fs2/StreamSpec.scala
+++ core/shared/src/test/scala/fs2/StreamSpec.scala
@@ -1461,10 +1461,118 @@ class StreamSpec extends Fs2Spec {
         }
       }
 
-      "left/right failure" in {
-        forAll { (s1: Stream[Pure, Int]) =>
+      "left/right failure" - {
+        "1" in forAll { (s1: Stream[Pure, Int]) =>
           s1.covary[IO].merge(Stream.raiseError[IO](new Err)).compile.drain.assertThrows[Err]
         }
+
+        "2 - never-ending flatMap, failure after emit" in forAll { (s1: Stream[Pure, Int]) =>
+          s1.merge(Stream.raiseError[IO](new Err))
+            .evalMap(_ => IO.never)
+            .compile
+            .drain
+            .assertThrows[Err]
+        }
+
+        "3 - constant flatMap, failure after emit" in {
+          pending
+          forAll { (s1: Stream[Pure, Int]) =>
+            s1.merge(Stream.raiseError[IO](new Err))
+              .flatMap(_ => Stream.constant(true))
+              .compile
+              .drain
+              .assertThrows[Err]
+          }
+        }
+      }
+
+      "run finalizers of inner streams first" in forAll {
+        (s: Stream[Pure, Int], leftBiased: Boolean) =>
+          // tests that finalizers of inner stream are always run before outer finalizer
+          // also this will test that when the either side throws an exception in finalizer it is caught
+          val err = new Err
+          Ref.of[IO, List[String]](Nil).flatMap { finalizerRef =>
+            Ref.of[IO, (Boolean, Boolean)]((false, false)).flatMap { sideRunRef =>
+              Deferred[IO, Unit].flatMap { halt =>
+                def bracketed =
+                  Stream.bracket(IO.unit)(
+                    _ => finalizerRef.update(_ :+ "Outer")
+                  )
+
+                def register(side: String): IO[Unit] =
+                  sideRunRef.update {
+                    case (left, right) =>
+                      if (side == "L") (true, right)
+                      else (left, true)
+                  }
+
+                def finalizer(side: String): IO[Unit] =
+                  // this introduces delay and failure based on bias of the test
+                  if (leftBiased && side == "L")
+                    IO.sleep(100.millis) >> finalizerRef.update(_ :+ s"Inner $side") >> IO
+                      .raiseError(err)
+                  else if (!leftBiased && side == "R")
+                    IO.sleep(100.millis) >> finalizerRef.update(_ :+ s"Inner $side") >> IO
+                      .raiseError(err)
+                  else IO.sleep(50.millis) >> finalizerRef.update(_ :+ s"Inner $side")
+
+                val prg0 =
+                  bracketed
+                    .flatMap { b =>
+                      (Stream.bracket(register("L"))(_ => finalizer("L")) >> s)
+                        .merge(
+                          Stream.bracket(register("R"))(_ => finalizer("R")) >>
+                            Stream
+                              .eval(halt.complete(())) // immediately interrupt the outer stream
+                        )
+                    }
+                    .interruptWhen(halt.get.attempt)
+
+                prg0.compile.drain.attempt.flatMap { r =>
+                  finalizerRef.get.flatMap { finalizers =>
+                    sideRunRef.get.flatMap {
+                      case (left, right) =>
+                        if (left && right) IO {
+                          (finalizers should contain).allOf("Inner L", "Inner R", "Outer")
+                          finalizers.lastOption shouldBe Some("Outer")
+                          r shouldBe Left(err)
+                        } else if (left) IO {
+                          finalizers shouldBe List("Inner L", "Outer")
+                          if (leftBiased) r shouldBe Left(err)
+                          else r shouldBe Right(())
+                        } else if (right) IO {
+                          finalizers shouldBe List("Inner R", "Outer")
+                          if (!leftBiased) r shouldBe Left(err)
+                          else r shouldBe Right(())
+                        } else
+                          IO {
+                            finalizers shouldBe List("Outer")
+                            r shouldBe Right(())
+                          }
+
+                    }
+                  }
+                }
+              }
+            }
+          }
+      }
+
+      "hangs" - {
+        val full = Stream.constant(42)
+        val hang = Stream.repeatEval(IO.async[Unit](cb => ()))
+        val hang2: Stream[IO, Nothing] = full.drain
+        val hang3: Stream[IO, Nothing] =
+          Stream
+            .repeatEval[IO, Unit](IO.async[Unit](cb => cb(Right(()))) >> IO.shift)
+            .drain
+
+        "1" in { full.merge(hang).take(1).compile.toList.asserting(_ shouldBe List(42)) }
+        "2" in { full.merge(hang2).take(1).compile.toList.asserting(_ shouldBe List(42)) }
+        "3" in { full.merge(hang3).take(1).compile.toList.asserting(_ shouldBe List(42)) }
+        "4" in { hang.merge(full).take(1).compile.toList.asserting(_ shouldBe List(42)) }
+        "5" in { hang2.merge(full).take(1).compile.toList.asserting(_ shouldBe List(42)) }
+        "6" in { hang3.merge(full).take(1).compile.toList.asserting(_ shouldBe List(42)) }
       }
     }
 
@@ -1640,6 +1748,153 @@ class StreamSpec extends Fs2Spec {
             .toList
             .asserting(r => (r should have).length(0))
         }
+      }
+    }
+
+    "parJoin" - {
+
+      "no concurrency" in forAll { (s: Stream[Pure, Int]) =>
+        s.covary[IO]
+          .map(Stream.emit(_).covary[IO])
+          .parJoin(1)
+          .compile
+          .toList
+          .asserting(_.toSet shouldBe s.toList.toSet)
+      }
+
+      "concurrency" in forAll { (s: Stream[Pure, Int], n0: PosInt) =>
+        val n = n0 % 20 + 1
+        s.covary[IO]
+          .map(Stream.emit(_).covary[IO])
+          .parJoin(n)
+          .compile
+          .toList
+          .asserting(_.toSet shouldBe s.toList.toSet)
+      }
+
+      "concurrent flattening" in forAll { (s: Stream[Pure, Stream[Pure, Int]], n0: PosInt) =>
+        val n = n0 % 20 + 1
+        s.map(_.covary[IO])
+          .covary[IO]
+          .parJoin(n)
+          .compile
+          .toList
+          .asserting(_.toSet shouldBe s.flatten.toList.toSet)
+      }
+
+      "merge consistency" in forAll { (s1: Stream[Pure, Int], s2: Stream[Pure, Int]) =>
+        val parJoined = Stream(s1.covary[IO], s2).parJoin(2).compile.toList.map(_.toSet)
+        val merged = s1.covary[IO].merge(s2).compile.toList.map(_.toSet)
+        (parJoined, merged).tupled.asserting { case (pj, m) => pj shouldBe m }
+      }
+
+      "resources acquired in outer stream are released after inner streams complete" in {
+        pending // Broken
+        val bracketed =
+          Stream.bracket(IO(new java.util.concurrent.atomic.AtomicBoolean(true)))(b =>
+            IO(b.set(false)))
+        // Starts an inner stream which fails if the resource b is finalized
+        val s: Stream[IO, Stream[IO, Unit]] = bracketed.map { b =>
+          Stream
+            .eval(IO(b.get))
+            .flatMap(b => if (b) Stream(()) else Stream.raiseError[IO](new Err))
+            .repeat
+            .take(10000)
+        }
+        s.parJoinUnbounded.compile.drain.assertNoException
+      }
+
+      "run finalizers of inner streams first" in {
+        pending // Broken
+        forAll { (s1: Stream[Pure, Int], bias: Boolean) =>
+          val err = new Err
+          val biasIdx = if (bias) 1 else 0
+          Ref
+            .of[IO, List[String]](Nil)
+            .flatMap { finalizerRef =>
+              Ref.of[IO, List[Int]](Nil).flatMap { runEvidenceRef =>
+                Deferred[IO, Unit].flatMap { halt =>
+                  def bracketed =
+                    Stream.bracket(IO.unit)(
+                      _ => finalizerRef.update(_ :+ "Outer")
+                    )
+
+                  def registerRun(idx: Int): IO[Unit] =
+                    runEvidenceRef.update(_ :+ idx)
+
+                  def finalizer(idx: Int): IO[Unit] =
+                    // this introduces delay and failure based on bias of the test
+                    if (idx == biasIdx) {
+                      IO.sleep(100.millis) >>
+                        finalizerRef.update(_ :+ s"Inner $idx") >>
+                        IO.raiseError(err)
+                    } else {
+                      finalizerRef.update(_ :+ s"Inner $idx")
+                    }
+
+                  val prg0 =
+                    bracketed.flatMap { _ =>
+                      Stream(
+                        Stream.bracket(registerRun(0))(_ => finalizer(0)) >> s1,
+                        Stream.bracket(registerRun(1))(_ => finalizer(1)) >> Stream.eval_(
+                          halt.complete(()))
+                      )
+                    }
+
+                  prg0.parJoinUnbounded.compile.drain.attempt.flatMap { r =>
+                    finalizerRef.get.flatMap { finalizers =>
+                      runEvidenceRef.get.flatMap { streamRunned =>
+                        IO {
+                          val expectedFinalizers = (streamRunned.map { idx =>
+                            s"Inner $idx"
+                          }) :+ "Outer"
+                          (finalizers should contain).theSameElementsAs(expectedFinalizers)
+                          finalizers.lastOption shouldBe Some("Outer")
+                          if (streamRunned.contains(biasIdx)) r shouldBe Left(err)
+                          else r shouldBe Right(())
+                        }
+                      }
+                    }
+                  }
+
+                }
+              }
+            }
+            .assertNoException
+        }
+      }
+
+      "hangs" - {
+        val full = Stream.constant(42)
+        val hang = Stream.repeatEval(IO.async[Unit](cb => ()))
+        val hang2: Stream[IO, Nothing] = full.drain
+        val hang3: Stream[IO, Nothing] =
+          Stream
+            .repeatEval[IO, Unit](IO.async[Unit](cb => cb(Right(()))) >> IO.shift)
+            .drain
+
+        "1" in {
+          Stream(full, hang).parJoin(10).take(1).compile.toList.asserting(_ shouldBe List(42))
+        }
+        "2" in {
+          Stream(full, hang2).parJoin(10).take(1).compile.toList.asserting(_ shouldBe List(42))
+        }
+        "3" in {
+          Stream(full, hang3).parJoin(10).take(1).compile.toList.asserting(_ shouldBe List(42))
+        }
+        "4" in {
+          Stream(hang3, hang2, full)
+            .parJoin(10)
+            .take(1)
+            .compile
+            .toList
+            .asserting(_ shouldBe List(42))
+        }
+      }
+
+      "outer failed" in {
+        Stream(Stream.sleep_[IO](1.minute), Stream.raiseError[IO](new Err)).parJoinUnbounded.compile.drain
+          .assertThrows[Err]
       }
     }
 
