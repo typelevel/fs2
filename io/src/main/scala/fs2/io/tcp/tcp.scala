ddd7a53092e8a165b5283ff5be650679b3671fbb
GabrielAsman <gabriel.asman@ovoenergy.com>
2019-08-05 12:11:38 +0100

    Additional socket options.

--- io/src/main/scala/fs2/io/tcp/tcp.scala
+++ io/src/main/scala/fs2/io/tcp/tcp.scala
@@ -1,5 +1,9 @@
 package fs2
 package io
 
+import java.net.SocketOption
+
 /** Provides support for TCP networking. */
-package object tcp
+package object tcp {
+  type SocketMapping[A] = (SocketOption[A], A)
+}
