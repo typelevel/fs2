58ff8bfd84802d9f4442199530f1075631ff9834
Christopher Davenport <chris@christopherdavenport.tech>
2019-09-08 11:03:58 -0700

    Add Missing Paren

--- core/shared/src/main/scala/fs2/Stream.scala
+++ core/shared/src/main/scala/fs2/Stream.scala
@@ -3526,7 +3526,7 @@ object Stream extends StreamLowPriority {
     * if it is Some, or terminating on None
     *
     * @example {{{
-    * scala> Stream.unfoldLoop(0)(i => (i, if (i < 5) Some(i+1) else None).toList
+    * scala> Stream.unfoldLoop(0)(i => (i, if (i < 5) Some(i+1) else None)).toList
     * res0: List[Int] = List(0, 1, 2, 3, 4, 5)
     * }}}
     */
