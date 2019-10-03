eb13341acd49581ae02df25e3a95f8c2465150de
Gavin Bisesi <gbisesi@teikametrics.com>
2019-10-03 10:41:44 -0400

    Add `readOutputStream` combinator in fs2-io

--- io/src/test/scala/fs2/io/IoSpec.scala
+++ io/src/test/scala/fs2/io/IoSpec.scala
@@ -1,8 +1,11 @@
 package fs2.io
 
-import java.io.{ByteArrayInputStream, InputStream}
-import cats.effect.{Blocker, IO}
+import java.io.{ByteArrayInputStream, InputStream, OutputStream}
+import java.util.concurrent.Executors
+import cats.effect.{Blocker, ContextShift, IO, Resource}
+import cats.implicits._
 import fs2.Fs2Spec
+import scala.concurrent.ExecutionContext
 
 class IoSpec extends Fs2Spec {
   "readInputStream" - {
@@ -22,6 +25,64 @@ class IoSpec extends Fs2Spec {
           val stream = readInputStream(IO(is), chunkSize, blocker)
           stream.buffer(chunkSize * 2).compile.toVector.asserting(_.toArray shouldBe bytes)
         }
+    }
+  }
+
+  "readOutputStream" - {
+    "writes data and terminates when `f` returns" in forAll(
+      arrayGenerator[Byte],
+      intsBetween(1, 20)
+    ) { (bytes: Array[Byte], chunkSize: Int) =>
+      Blocker[IO].use { blocker =>
+        readOutputStream[IO](blocker, chunkSize)(
+          (os: OutputStream) => IO(os.write(bytes))
+        ).compile.to[Array]
+          .asserting(_ shouldEqual bytes)
+      }
+    }
+
+    "can be manually closed from inside `f`" in forAll(intsBetween(1, 20)) {
+      chunkSize: Int =>
+        Blocker[IO].use { blocker =>
+          readOutputStream[IO](blocker, chunkSize)(
+            (os: OutputStream) => IO(os.close()) *> IO.never
+          ).compile.toVector
+            .asserting(_ shouldBe Vector.empty)
+        }
+    }
+
+    "fails when `f` fails" in forAll(intsBetween(1, 20)) { chunkSize: Int =>
+        val e = new Exception("boom")
+        Blocker[IO].use { blocker =>
+          readOutputStream[IO](blocker, chunkSize)((_: OutputStream) => IO.raiseError(e)).compile.toVector.attempt
+            .asserting(_ shouldBe Left(e))
+        }
+    }
+
+    "Doesn't deadlock with size-1 ContextShift thread pool" in {
+      val pool = Resource
+        .make(IO(Executors.newFixedThreadPool(1)))(ec => IO(ec.shutdown()))
+        .map(ExecutionContext.fromExecutor)
+        .map(IO.contextShift)
+      def write(os: OutputStream): IO[Unit] = IO {
+        os.write(1)
+        os.write(1)
+        os.write(1)
+        os.write(1)
+        os.write(1)
+        os.write(1)
+      }
+      Blocker[IO].use { blocker =>
+        // Note: name `contextShiftIO` is important because it shadows the outer implicit, preventing ambiguity
+        pool
+          .use { implicit contextShiftIO: ContextShift[IO] =>
+            readOutputStream[IO](blocker, chunkSize = 1)(write)
+              .take(5)
+              .compile
+              .toVector
+          }
+          .asserting(_ should have size 5)
+      }
     }
   }
 
