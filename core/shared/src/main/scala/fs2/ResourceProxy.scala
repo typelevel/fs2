f126ee752fdcc76ab71f263c67e4ed278ba2833c
Fabio Labella <fabio.labella2@gmail.com>
2019-10-25 01:08:41 +0100

    inline definition of ResourceProxy

--- core/shared/src/main/scala/fs2/ResourceProxy.scala
+++ core/shared/src/main/scala/fs2/ResourceProxy.scala
@@ -1,7 +1,7 @@
 package fs2
 
 import cats.implicits._
-import cats.effect.{Bracket, Resource, Sync}
+import cats.effect.{Resource, Sync}
 import cats.effect.concurrent.Ref
 import cats.effect.implicits._
 
@@ -12,46 +12,40 @@ trait ResourceProxy[F[_], R] {
 
 object ResourceProxy {
   def apply[F[_]: Sync, R](initial: Resource[F, R]): Resource[F, ResourceProxy[F, R]] = {
+
     val acquire = for {
       v <- initial.allocated
-      state <- Ref.of[F, Option[(R, F[Unit])]](Some(v))
-    } yield new ResourceProxyImpl(state)
+      state <- Ref.of[F, Option[(R, F[Unit])]](v.some)
+    } yield state
 
-    Resource.make(acquire)(_.runFinalizer).map { impl =>
-      new ResourceProxy[F, R] {
-        def get: F[R] = impl.get
-        def swap(next: Resource[F, R]): F[R] = impl.swap(next)
-      }
-    }
-  }
-
-  final class ResourceProxyImpl[F[_], R](state: Ref[F, Option[(R, F[Unit])]])(
-      implicit F: Bracket[F, Throwable]
-  ) {
-
-    def raise[A](msg: String): F[A] = F.raiseError(new RuntimeException(msg))
-
-    def get: F[R] = state.get.flatMap {
-      case None         => raise("Cannot get after proxy has been finalized")
-      case Some((r, _)) => r.pure[F]
-    }
-
-    // Runs the finalizer for the current proxy target and delays finalizer of newValue until this proxy is finalized
-    def swap(next: Resource[F, R]): F[R] =
-      next.allocated.flatMap {
-        case next @ (newValue, newFinalizer) =>
-          state.modify {
-            case Some((_, oldFinalizer)) =>
-              next.some -> oldFinalizer.as(newValue)
-            case None =>
-              None -> (newFinalizer *> raise[R]("Cannot swap after proxy has been finalized"))
-          }.flatten
-      }.uncancelable
-
-    def runFinalizer: F[Unit] =
+    def runFinalizer(state: Ref[F, Option[(R, F[Unit])]]): F[Unit] =
       state.modify {
         case None                 => None -> raise[Unit]("Finalizer already run")
         case Some((_, finalizer)) => None -> finalizer
       }.flatten
+
+    def raise[A](msg: String): F[A] = Sync[F].raiseError(new RuntimeException(msg))
+
+    Resource.make(acquire)(runFinalizer(_)).map { state =>
+      new ResourceProxy[F, R] {
+        def get: F[R] = state.get.flatMap {
+          case None         => raise("Cannot get after proxy has been finalized")
+          case Some((r, _)) => r.pure[F]
+        }
+
+        // Runs the finalizer for the current proxy target and delays finalizer of newValue until this proxy is finalized
+        def swap(next: Resource[F, R]): F[R] =
+          next.allocated.flatMap {
+            case next @ (newValue, newFinalizer) =>
+              state.modify {
+                case Some((_, oldFinalizer)) =>
+                  next.some -> oldFinalizer.as(newValue)
+                case None =>
+                  None -> (newFinalizer *> raise[R]("Cannot swap after proxy has been finalized"))
+              }.flatten
+          }.uncancelable
+
+      }
+    }
   }
 }
