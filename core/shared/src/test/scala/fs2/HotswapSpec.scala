358d9cdfa566f3d238bf938d078b43e893cc2ff7
Michael Pilquist <mpilquist@gmail.com>
2019-11-04 20:55:07 -0500

    Improved error message when not using stepLeg for merging streams and added some test cases around zips and scopes

--- core/shared/src/test/scala/fs2/HotswapSpec.scala
+++ core/shared/src/test/scala/fs2/HotswapSpec.scala
@@ -1,105 +1,77 @@
 package fs2
 
 import cats.implicits._
-import cats.effect.Sync
-import cats.effect.concurrent.Ref
 import cats.effect.IO
-import cats.effect.Resource
 
 class HotswapSpec extends Fs2Spec {
   "Hotswap" - {
     "finalizer of target run when hotswap is finalized" in {
-      mkEventLogger.flatMap { logger =>
+      Logger[IO].flatMap { logger =>
         Stream
-          .resource(Hotswap(logLifecycle(logger, "a")))
+          .resource(Hotswap(logger.logLifecycleR("a")))
           .flatMap { _ =>
-            Stream
-              .eval_(logger.log(Info("using")))
+            logger.logInfo("using")
           }
           .compile
           .drain *> logger.get.asserting(
           _ shouldBe List(
-            Acquired("a"),
-            Info("using"),
-            Released("a")
+            LogEvent.Acquired("a"),
+            LogEvent.Info("using"),
+            LogEvent.Released("a")
           )
         )
       }
     }
 
     "swap acquires new resource and then finalizes old resource" in {
-      mkEventLogger.flatMap { logger =>
+      Logger[IO].flatMap { logger =>
         Stream
-          .resource(Hotswap(logLifecycle(logger, "a")))
+          .resource(Hotswap(logger.logLifecycleR("a")))
           .flatMap {
             case (hotswap, _) =>
-              Stream.eval_(logger.log(Info("using a"))) ++
-                Stream.eval_(hotswap.swap(logLifecycle(logger, "b"))) ++
-                Stream.eval_(logger.log(Info("using b"))) ++
-                Stream.eval_(hotswap.swap(logLifecycle(logger, "c"))) ++
-                Stream.eval_(logger.log(Info("using c")))
+              logger.logInfo("using a") ++
+                Stream.eval_(hotswap.swap(logger.logLifecycleR("b"))) ++
+                logger.logInfo("using b") ++
+                Stream.eval_(hotswap.swap(logger.logLifecycleR("c"))) ++
+                logger.logInfo("using c")
           }
           .compile
           .drain *> logger.get.asserting(
           _ shouldBe List(
-            Acquired("a"),
-            Info("using a"),
-            Acquired("b"),
-            Released("a"),
-            Info("using b"),
-            Acquired("c"),
-            Released("b"),
-            Info("using c"),
-            Released("c")
+            LogEvent.Acquired("a"),
+            LogEvent.Info("using a"),
+            LogEvent.Acquired("b"),
+            LogEvent.Released("a"),
+            LogEvent.Info("using b"),
+            LogEvent.Acquired("c"),
+            LogEvent.Released("b"),
+            LogEvent.Info("using c"),
+            LogEvent.Released("c")
           )
         )
       }
     }
 
     "clear finalizes old resource" in {
-      mkEventLogger.flatMap { logger =>
+      Logger[IO].flatMap { logger =>
         Stream
-          .resource(Hotswap(logLifecycle(logger, "a")))
+          .resource(Hotswap(logger.logLifecycleR("a")))
           .flatMap {
             case (hotswap, _) =>
-              Stream.eval_(logger.log(Info("using a"))) ++
+              logger.logInfo("using a") ++
                 Stream.eval_(hotswap.clear) ++
-                Stream.eval_(logger.log(Info("after clear")))
+                logger.logInfo("after clear")
           }
           .compile
           .drain *> logger.get.asserting(
           _ shouldBe List(
-            Acquired("a"),
-            Info("using a"),
-            Released("a"),
-            Info("after clear")
+            LogEvent.Acquired("a"),
+            LogEvent.Info("using a"),
+            LogEvent.Released("a"),
+            LogEvent.Info("after clear")
           )
         )
       }
     }
   }
-
-  trait Logger[F[_], A] {
-    def log(a: A): F[Unit]
-    def get: F[List[A]]
-  }
-  object Logger {
-    def apply[F[_]: Sync, A]: F[Logger[F, A]] =
-      Ref.of(Nil: List[A]).map { ref =>
-        new Logger[F, A] {
-          def log(a: A): F[Unit] = ref.update(acc => a :: acc)
-          def get: F[List[A]] = ref.get.map(_.reverse)
-        }
-      }
-  }
-
-  sealed trait Event
-  case class Acquired(tag: String) extends Event
-  case class Released(tag: String) extends Event
-  case class Info(message: String) extends Event
-
-  def mkEventLogger: IO[Logger[IO, Event]] = Logger[IO, Event]
-
-  def logLifecycle(logger: Logger[IO, Event], tag: String): Resource[IO, Unit] =
-    Resource.make(logger.log(Acquired(tag)))(_ => logger.log(Released(tag)))
 }
