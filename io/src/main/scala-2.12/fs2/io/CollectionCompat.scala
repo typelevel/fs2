e1507d979b7ec6ec19b258dac097e238927c4b0a
Michael Pilquist <mpilquist@gmail.com>
2020-01-03 11:01:30 -0500

    Fixed 2.12 build

--- io/src/main/scala-2.12/fs2/io/CollectionCompat.scala
+++ io/src/main/scala-2.12/fs2/io/CollectionCompat.scala
@@ -4,12 +4,12 @@ import scala.collection.JavaConverters._
 
 private[fs2] object CollectionCompat {
   implicit class JIteratorOps[A](private val self: java.util.Iterator[A]) extends AnyVal {
-    def asScala: Iterator[A] = IteratorHasAsScala(self).asScala
+    def asScala: Iterator[A] = asScalaIterator(self)
   }
   implicit class JIterableOps[A](private val self: java.lang.Iterable[A]) extends AnyVal {
-    def asScala: Iterable[A] = IterableHasAsScala(self).asScala
+    def asScala: Iterable[A] = iterableAsScalaIterable(self)
   }
   implicit class ListOps[A](private val self: List[A]) extends AnyVal {
-    def asJava: java.util.List[A] = SeqHasAsJava(self).asJava
+    def asJava: java.util.List[A] = seqAsJavaList(self)
   }
 }
