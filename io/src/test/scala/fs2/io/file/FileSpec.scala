16d66e9d9ab22266c83a4409623ff0352f18ec63
mgibowski <gibowski@gmail.com>
2020-01-31 11:00:36 +0000

    Replace remaining ShouldMatchers in FileSpec

--- io/src/test/scala/fs2/io/file/FileSpec.scala
+++ io/src/test/scala/fs2/io/file/FileSpec.scala
@@ -15,147 +15,170 @@ import scala.concurrent.duration._
 class FileSpec extends BaseFileSpec {
   "readAll" - {
     "retrieves whole content of a file" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { bec =>
-          tempFile
-            .flatTap(modify)
-            .flatMap(path => file.readAll[IO](path, bec, 4096))
-        }
-        .compile
-        .toList
-        .map(_.size)
-        .unsafeRunSync() shouldBe 4
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { bec =>
+            tempFile
+              .flatTap(modify)
+              .flatMap(path => file.readAll[IO](path, bec, 4096))
+          }
+          .compile
+          .toList
+          .map(_.size)
+          .unsafeRunSync() == 4
+      )
     }
   }
 
   "readRange" - {
     "reads half of a file" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { bec =>
-          tempFile
-            .flatTap(modify)
-            .flatMap(path => file.readRange[IO](path, bec, 4096, 0, 2))
-        }
-        .compile
-        .toList
-        .map(_.size)
-        .unsafeRunSync() shouldBe 2
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { bec =>
+            tempFile
+              .flatTap(modify)
+              .flatMap(path => file.readRange[IO](path, bec, 4096, 0, 2))
+          }
+          .compile
+          .toList
+          .map(_.size)
+          .unsafeRunSync() == 2
+      )
     }
 
     "reads full file if end is bigger than file size" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { bec =>
-          tempFile
-            .flatTap(modify)
-            .flatMap(path => file.readRange[IO](path, bec, 4096, 0, 100))
-        }
-        .compile
-        .toList
-        .map(_.size)
-        .unsafeRunSync() shouldBe 4
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { bec =>
+            tempFile
+              .flatTap(modify)
+              .flatMap(path => file.readRange[IO](path, bec, 4096, 0, 100))
+          }
+          .compile
+          .toList
+          .map(_.size)
+          .unsafeRunSync() == 4
+      )
     }
   }
 
   "writeAll" - {
     "simple write" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { bec =>
-          tempFile
-            .flatMap(path =>
-              Stream("Hello", " world!")
-                .covary[IO]
-                .through(text.utf8Encode)
-                .through(file.writeAll[IO](path, bec))
-                .drain ++ file.readAll[IO](path, bec, 4096).through(text.utf8Decode)
-            )
-        }
-        .compile
-        .foldMonoid
-        .unsafeRunSync() shouldBe "Hello world!"
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { bec =>
+            tempFile
+              .flatMap(path =>
+                Stream("Hello", " world!")
+                  .covary[IO]
+                  .through(text.utf8Encode)
+                  .through(file.writeAll[IO](path, bec))
+                  .drain ++ file.readAll[IO](path, bec, 4096).through(text.utf8Decode)
+              )
+          }
+          .compile
+          .foldMonoid
+          .unsafeRunSync() == "Hello world!"
+      )
     }
 
     "append" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { bec =>
-          tempFile
-            .flatMap { path =>
-              val src = Stream("Hello", " world!").covary[IO].through(text.utf8Encode)
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { bec =>
+            tempFile
+              .flatMap { path =>
+                val src = Stream("Hello", " world!").covary[IO].through(text.utf8Encode)
 
-              src.through(file.writeAll[IO](path, bec)).drain ++
-                src.through(file.writeAll[IO](path, bec, List(StandardOpenOption.APPEND))).drain ++
-                file.readAll[IO](path, bec, 4096).through(text.utf8Decode)
-            }
-        }
-        .compile
-        .foldMonoid
-        .unsafeRunSync() shouldBe "Hello world!Hello world!"
+                src.through(file.writeAll[IO](path, bec)).drain ++
+                  src
+                    .through(file.writeAll[IO](path, bec, List(StandardOpenOption.APPEND)))
+                    .drain ++
+                  file.readAll[IO](path, bec, 4096).through(text.utf8Decode)
+              }
+          }
+          .compile
+          .foldMonoid
+          .unsafeRunSync() == "Hello world!Hello world!"
+      )
     }
   }
 
   "tail" - {
     "keeps reading a file as it is appended" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempFile
-            .flatMap { path =>
-              file
-                .tail[IO](path, blocker, 4096, pollDelay = 25.millis)
-                .concurrently(modifyLater(path, blocker))
-            }
-        }
-        .take(4)
-        .compile
-        .toList
-        .map(_.size)
-        .unsafeRunSync() shouldBe 4
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempFile
+              .flatMap { path =>
+                file
+                  .tail[IO](path, blocker, 4096, pollDelay = 25.millis)
+                  .concurrently(modifyLater(path, blocker))
+              }
+          }
+          .take(4)
+          .compile
+          .toList
+          .map(_.size)
+          .unsafeRunSync() == 4
+      )
     }
   }
 
   "exists" - {
     "returns false on a non existent file" in {
-      Blocker[IO].use(b => file.exists[IO](b, Paths.get("nothing"))).unsafeRunSync shouldBe false
+      assert(Blocker[IO].use(b => file.exists[IO](b, Paths.get("nothing"))).unsafeRunSync == false)
     }
     "returns true on an existing file" in {
-      Blocker[IO]
-        .use(b => tempFile.evalMap(file.exists[IO](b, _)).compile.fold(true)(_ && _))
-        .unsafeRunSync() shouldBe true
+      assert(
+        Blocker[IO]
+          .use(b => tempFile.evalMap(file.exists[IO](b, _)).compile.fold(true)(_ && _))
+          .unsafeRunSync() == true
+      )
     }
   }
 
   "permissions" - {
     "should fail for a non existent file" in {
-      Blocker[IO]
-        .use(b => file.permissions[IO](b, Paths.get("nothing")))
-        .attempt
-        .unsafeRunSync()
-        .isLeft shouldBe true
+      assert(
+        Blocker[IO]
+          .use(b => file.permissions[IO](b, Paths.get("nothing")))
+          .attempt
+          .unsafeRunSync()
+          .isLeft == true
+      )
     }
     "should return permissions for existing file" in {
       val permissions = PosixFilePermissions.fromString("rwxrwxr-x").asScala
-      Blocker[IO]
-        .use { b =>
-          tempFile
-            .evalMap(p => file.setPermissions[IO](b, p, permissions) >> file.permissions[IO](b, p))
-            .compile
-            .lastOrError
-        }
-        .unsafeRunSync() shouldBe permissions
+      assert(
+        Blocker[IO]
+          .use { b =>
+            tempFile
+              .evalMap(p => file.setPermissions[IO](b, p, permissions) >> file.permissions[IO](b, p)
+              )
+              .compile
+              .lastOrError
+          }
+          .unsafeRunSync() == permissions
+      )
     }
   }
 
   "setPermissions" - {
     "should fail for a non existent file" in {
-      Blocker[IO]
-        .use(b => file.setPermissions[IO](b, Paths.get("nothing"), Set.empty))
-        .attempt
-        .unsafeRunSync()
-        .isLeft shouldBe true
+      assert(
+        Blocker[IO]
+          .use(b => file.setPermissions[IO](b, Paths.get("nothing"), Set.empty))
+          .attempt
+          .unsafeRunSync()
+          .isLeft == true
+      )
     }
     "should correctly change file permissions for existing file" in {
       val permissions = PosixFilePermissions.fromString("rwxrwxr-x").asScala
@@ -174,188 +197,212 @@ class FileSpec extends BaseFileSpec {
         }
         .unsafeRunSync()
 
-      initial should not be updated
+      assert(initial != updated)
       assert(updated == permissions)
     }
   }
 
   "copy" - {
     "returns a path to the new file" in {
-      (for {
-        blocker <- Stream.resource(Blocker[IO])
-        filePath <- tempFile
-        tempDir <- tempDirectory
-        result <- Stream.eval(file.copy[IO](blocker, filePath, tempDir.resolve("newfile")))
-        exists <- Stream.eval(file.exists[IO](blocker, result))
-      } yield exists).compile.fold(true)(_ && _).unsafeRunSync() shouldBe true
+      assert(
+        (for {
+          blocker <- Stream.resource(Blocker[IO])
+          filePath <- tempFile
+          tempDir <- tempDirectory
+          result <- Stream.eval(file.copy[IO](blocker, filePath, tempDir.resolve("newfile")))
+          exists <- Stream.eval(file.exists[IO](blocker, result))
+        } yield exists).compile.fold(true)(_ && _).unsafeRunSync() == true
+      )
     }
   }
 
   "deleteIfExists" - {
     "should result in non existent file" in {
-      tempFile
-        .flatMap(path =>
-          Stream
-            .resource(Blocker[IO])
-            .evalMap(blocker => file.delete[IO](blocker, path) *> file.exists[IO](blocker, path))
-        )
-        .compile
-        .fold(false)(_ || _)
-        .unsafeRunSync() shouldBe false
+      assert(
+        tempFile
+          .flatMap(path =>
+            Stream
+              .resource(Blocker[IO])
+              .evalMap(blocker => file.delete[IO](blocker, path) *> file.exists[IO](blocker, path))
+          )
+          .compile
+          .fold(false)(_ || _)
+          .unsafeRunSync() == false
+      )
     }
   }
 
   "delete" - {
     "should fail on a non existent file" in {
-      Blocker[IO]
-        .use(blocker => file.delete[IO](blocker, Paths.get("nothing")))
-        .attempt
-        .unsafeRunSync()
-        .isLeft shouldBe true
+      assert(
+        Blocker[IO]
+          .use(blocker => file.delete[IO](blocker, Paths.get("nothing")))
+          .attempt
+          .unsafeRunSync()
+          .isLeft == true
+      )
     }
   }
 
   "move" - {
     "should result in the old path being deleted" in {
-      (for {
-        blocker <- Stream.resource(Blocker[IO])
-        filePath <- tempFile
-        tempDir <- tempDirectory
-        _ <- Stream.eval(file.move[IO](blocker, filePath, tempDir.resolve("newfile")))
-        exists <- Stream.eval(file.exists[IO](blocker, filePath))
-      } yield exists).compile.fold(false)(_ || _).unsafeRunSync() shouldBe false
+      assert(
+        (for {
+          blocker <- Stream.resource(Blocker[IO])
+          filePath <- tempFile
+          tempDir <- tempDirectory
+          _ <- Stream.eval(file.move[IO](blocker, filePath, tempDir.resolve("newfile")))
+          exists <- Stream.eval(file.exists[IO](blocker, filePath))
+        } yield exists).compile.fold(false)(_ || _).unsafeRunSync() == false
+      )
     }
   }
 
   "size" - {
     "should return correct size of ay file" in {
-      tempFile
-        .flatTap(modify)
-        .flatMap(path =>
-          Stream
-            .resource(Blocker[IO])
-            .evalMap(blocker => file.size[IO](blocker, path))
-        )
-        .compile
-        .lastOrError
-        .unsafeRunSync() shouldBe 4L
+      assert(
+        tempFile
+          .flatTap(modify)
+          .flatMap(path =>
+            Stream
+              .resource(Blocker[IO])
+              .evalMap(blocker => file.size[IO](blocker, path))
+          )
+          .compile
+          .lastOrError
+          .unsafeRunSync() == 4L
+      )
     }
   }
 
   "createDirectory" - {
     "should return in an existing path" in {
-      tempDirectory
-        .flatMap(path =>
-          Stream
-            .resource(Blocker[IO])
-            .evalMap(blocker =>
-              file
-                .createDirectory[IO](blocker, path.resolve("temp"))
-                .bracket(file.exists[IO](blocker, _))(file.deleteIfExists[IO](blocker, _).void)
-            )
-        )
-        .compile
-        .fold(true)(_ && _)
-        .unsafeRunSync() shouldBe true
+      assert(
+        tempDirectory
+          .flatMap(path =>
+            Stream
+              .resource(Blocker[IO])
+              .evalMap(blocker =>
+                file
+                  .createDirectory[IO](blocker, path.resolve("temp"))
+                  .bracket(file.exists[IO](blocker, _))(file.deleteIfExists[IO](blocker, _).void)
+              )
+          )
+          .compile
+          .fold(true)(_ && _)
+          .unsafeRunSync() == true
+      )
     }
   }
 
   "createDirectories" - {
     "should return in an existing path" in {
-      tempDirectory
-        .flatMap(path =>
-          Stream
-            .resource(Blocker[IO])
-            .evalMap(blocker =>
-              file
-                .createDirectories[IO](blocker, path.resolve("temp/inner"))
-                .bracket(file.exists[IO](blocker, _))(file.deleteIfExists[IO](blocker, _).void)
-            )
-        )
-        .compile
-        .fold(true)(_ && _)
-        .unsafeRunSync() shouldBe true
+      assert(
+        tempDirectory
+          .flatMap(path =>
+            Stream
+              .resource(Blocker[IO])
+              .evalMap(blocker =>
+                file
+                  .createDirectories[IO](blocker, path.resolve("temp/inner"))
+                  .bracket(file.exists[IO](blocker, _))(file.deleteIfExists[IO](blocker, _).void)
+              )
+          )
+          .compile
+          .fold(true)(_ && _)
+          .unsafeRunSync() == true
+      )
     }
   }
 
   "directoryStream" - {
     "returns an empty Stream on an empty directory" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempDirectory
-            .flatMap { path =>
-              file.directoryStream[IO](blocker, path)
-            }
-        }
-        .compile
-        .toList
-        .unsafeRunSync()
-        .length shouldBe 0
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempDirectory
+              .flatMap { path =>
+                file.directoryStream[IO](blocker, path)
+              }
+          }
+          .compile
+          .toList
+          .unsafeRunSync()
+          .length == 0
+      )
     }
 
     "returns all files in a directory correctly" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempFiles(10)
-            .flatMap { paths =>
-              val parent = paths.head.getParent
-              file.directoryStream[IO](blocker, parent).tupleRight(paths)
-            }
-        }
-        .map { case (path, paths) => paths.exists(_.normalize === path.normalize) }
-        .compile
-        .fold(true)(_ & _)
-        .unsafeRunSync() shouldBe true
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempFiles(10)
+              .flatMap { paths =>
+                val parent = paths.head.getParent
+                file.directoryStream[IO](blocker, parent).tupleRight(paths)
+              }
+          }
+          .map { case (path, paths) => paths.exists(_.normalize === path.normalize) }
+          .compile
+          .fold(true)(_ & _)
+          .unsafeRunSync() == true
+      )
     }
   }
 
   "walk" - {
     "returns the only file in a directory correctly" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempFile
-            .flatMap { path =>
-              file.walk[IO](blocker, path.getParent).map(_.normalize === path.normalize)
-            }
-        }
-        .compile
-        .toList
-        .unsafeRunSync()
-        .length shouldBe 2 // the directory and the file
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempFile
+              .flatMap { path =>
+                file.walk[IO](blocker, path.getParent).map(_.normalize === path.normalize)
+              }
+          }
+          .compile
+          .toList
+          .unsafeRunSync()
+          .length == 2 // the directory and the file
+      )
     }
 
     "returns all files in a directory correctly" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempFiles(10)
-            .flatMap { paths =>
-              val parent = paths.head.getParent
-              file.walk[IO](blocker, parent).tupleRight(parent :: paths)
-            }
-        }
-        .map { case (path, paths) => paths.exists(_.normalize === path.normalize) }
-        .compile
-        .fold(true)(_ & _)
-        .unsafeRunSync() shouldBe true // the directory itself and the files
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempFiles(10)
+              .flatMap { paths =>
+                val parent = paths.head.getParent
+                file.walk[IO](blocker, parent).tupleRight(parent :: paths)
+              }
+          }
+          .map { case (path, paths) => paths.exists(_.normalize === path.normalize) }
+          .compile
+          .fold(true)(_ & _)
+          .unsafeRunSync() == true // the directory itself and the files
+      )
     }
 
     "returns all files in a nested tree correctly" in {
-      Stream
-        .resource(Blocker[IO])
-        .flatMap { blocker =>
-          tempFilesHierarchy
-            .flatMap { topDir =>
-              file.walk[IO](blocker, topDir)
-            }
-        }
-        .compile
-        .toList
-        .unsafeRunSync()
-        .length shouldBe 31 // the root + 5 children + 5 files per child directory
+      assert(
+        Stream
+          .resource(Blocker[IO])
+          .flatMap { blocker =>
+            tempFilesHierarchy
+              .flatMap { topDir =>
+                file.walk[IO](blocker, topDir)
+              }
+          }
+          .compile
+          .toList
+          .unsafeRunSync()
+          .length == 31 // the root + 5 children + 5 files per child directory
+      )
     }
   }
 
