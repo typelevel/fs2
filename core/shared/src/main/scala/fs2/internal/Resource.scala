770cd5361f2e0a6ea14e59355cb752058d467654
Michael Pilquist <mpilquist@gmail.com>
2020-02-18 08:06:28 -0500

    Manually adjusted some trailing comments for readability

--- core/shared/src/main/scala/fs2/internal/Resource.scala
+++ core/shared/src/main/scala/fs2/internal/Resource.scala
@@ -112,13 +112,13 @@ private[internal] object Resource {
 
       def release(ec: ExitCase[Throwable]): F[Either[Throwable, Unit]] =
         F.flatMap(state.modify { s =>
-          if (s.leases != 0)
-            (s.copy(open = false), None) // do not allow to run finalizer if there are leases open
-          else
-            (
-              s.copy(open = false, finalizer = None),
-              s.finalizer
-            ) // reset finalizer to None, will be run, it available, otherwise the acquire will take care of it
+          if (s.leases != 0) {
+            // do not allow to run finalizer if there are leases open
+            (s.copy(open = false), None)
+          } else {
+            // reset finalizer to None, will be run, it available, otherwise the acquire will take care of it
+            (s.copy(open = false, finalizer = None), s.finalizer)
+          }
         })(finalizer => finalizer.map(_(ec)).getOrElse(pru))
 
       def acquired(finalizer: ExitCase[Throwable] => F[Unit]): F[Either[Throwable, Boolean]] =
