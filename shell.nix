4ce3acd4987f3d0855a6971ed2e946aaff8215fa
Gabriel Volpe <volpegabriel@gmail.com>
2020-06-09 14:37:04 +0200

    Fix automated publishing of microsite (#1893)
    * attempt to fix 1
    
    * Explicitly declaring JAVA_HOME
    
    * Overriding Java version in Sbt derivation
    
    * Re-adding branches and paths
    
    * Fix automated publishing of microsite

--- shell.nix
+++ shell.nix
@@ -1,15 +1,30 @@
 let
+  # Override the java version of sbt package
+  config = {
+    packageOverrides = pkgs: rec {
+      sbt = pkgs.sbt.overrideAttrs (
+        old: rec {
+          version = "1.3.12";
+
+          patchPhase = ''
+            echo -java-home ${pkgs.openjdk11} >> conf/sbtopts
+          '';
+        }
+      );
+    };
+  };
+
   nixpkgs = fetchTarball {
     name   = "NixOS-unstable-08-06-2020";
     url    = "https://github.com/NixOS/nixpkgs-channels/archive/dcb64ea42e6.tar.gz";
     sha256 = "0i77sgs0gic6pwbkvk9lbpfshgizdrqyh18law2ji1409azc09w0";
   };
-  pkgs = import nixpkgs {};
+  pkgs = import nixpkgs { inherit config; };
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [
       jekyll # 4.1.0
       openjdk11 # 11.0.6-internal
-      sbt # 1.3.10
+      sbt # 1.3.12
     ];
   }
