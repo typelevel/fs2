fc764c7d6f712335c7f571a49682e1a3dc36bc6f
Timothy McCarthy <ebh042@gmail.com>
2020-06-13 17:12:02 +1000

    Add collector support for tagged ArraySeqs

--- core/shared/src/main/scala-2.13/fs2/CollectorPlatform.scala
+++ core/shared/src/main/scala-2.13/fs2/CollectorPlatform.scala
@@ -1,6 +1,8 @@
 package fs2
 
+import scala.collection.immutable.ArraySeq
 import scala.collection.{Factory, IterableFactory, MapFactory}
+import scala.reflect.ClassTag
 
 private[fs2] trait CollectorPlatform { self: Collector.type =>
   implicit def supportsFactory[A, C[_], B](
@@ -13,6 +15,14 @@ private[fs2] trait CollectorPlatform { self: Collector.type =>
   implicit def supportsMapFactory[K, V, C[_, _]](f: MapFactory[C]): Collector.Aux[(K, V), C[K, V]] =
     make(Builder.fromMapFactory(f))
 
+  implicit def supportsTaggedArraySeq[A : ClassTag](a: ArraySeq.type): Collector.Aux[A, ArraySeq[A]] = {
+    val _ = a
+    make(implicitly[ClassTag[A]] match {
+      case ClassTag.Byte => Builder.byteArray.mapResult(ArraySeq.unsafeWrapArray).asInstanceOf[Builder[A, ArraySeq[A]]]
+      case _ => Builder.taggedArraySeq[A]
+    })
+  }
+
   private[fs2] trait BuilderPlatform { self: Collector.Builder.type =>
     def fromFactory[A, C[_], B](f: Factory[A, C[B]]): Builder[A, C[B]] =
       fromBuilder(f.newBuilder)
@@ -22,5 +32,8 @@ private[fs2] trait CollectorPlatform { self: Collector.type =>
 
     def fromMapFactory[K, V, C[_, _]](f: MapFactory[C]): Builder[(K, V), C[K, V]] =
       fromBuilder(f.newBuilder)
+
+    def taggedArraySeq[A: ClassTag]: Builder[A, ArraySeq[A]] =
+      array[A].mapResult(ArraySeq.unsafeWrapArray)
   }
 }
