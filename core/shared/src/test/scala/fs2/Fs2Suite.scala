8fe8db52c87fff70b551152b6fd3b5f0f76ecde2
mpilquist <mpilquist@gmail.com>
2020-08-10 09:24:29 -0400

    Fix io tests

--- core/shared/src/test/scala/fs2/Fs2Suite.scala
+++ core/shared/src/test/scala/fs2/Fs2Suite.scala
@@ -2,7 +2,7 @@ package fs2
 
 import scala.concurrent.ExecutionContext
 
-import cats.effect.Sync
+import cats.effect.{ConcurrentEffect, IO, Sync}
 import cats.implicits._
 import munit.{CatsEffectSuite, Location, ScalaCheckEffectSuite}
 import org.typelevel.discipline.Laws
@@ -21,6 +21,10 @@ abstract class Fs2Suite
   override def munitFlakyOK = true
 
   override val munitExecutionContext: ExecutionContext = ExecutionContext.global
+
+  // This is not normally necessary but Dotty requires it
+  implicit val ioConcurrentEffect: ConcurrentEffect[IO] =
+    IO.ioConcurrentEffect(munitContextShift)
 
   /** Provides various ways to make test assertions on an `F[A]`. */
   implicit class Asserting[F[_], A](private val self: F[A]) {
