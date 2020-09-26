7d87b47172b1342cbe54f511cb53ef776bc1fa2d
Gabriel Volpe <volpegabriel@gmail.com>
2020-09-26 17:46:02 +0200

    Fix microsite CI build

--- shell.nix
+++ shell.nix
@@ -1,21 +1,7 @@
 { java ? "openjdk11" }:
 
 let
-  nixpkgs = fetchTarball {
-    name   = "nixos-unstable-2020-09-25";
-    url    = "https://github.com/NixOS/nixpkgs-channels/archive/72b9660dc18b.tar.gz";
-    sha256 = "1cqgpw263bz261bgz34j6hiawi4hi6smwp6981yz375fx0g6kmss";
-  };
-
-  config = {
-    packageOverrides = p: {
-      sbt = p.sbt.override {
-        jre = p.${java};
-      };
-    };
-  };
-
-  pkgs = import nixpkgs { inherit config; };
+  pkgs = import nix/pkgs.nix { inherit java; };
 in
   pkgs.mkShell {
     buildInputs = [
