20a0d1f0265594e06e28ca0397b8f8f9fe2d6b5a
Gabriel Volpe <volpegabriel@gmail.com>
2020-09-26 17:43:14 +0200

    Fix microsite CI build

--- shell.nix
+++ shell.nix
@@ -1,30 +1,26 @@
-let
-  # Override the java version of sbt package
-  config = {
-    packageOverrides = pkgs: rec {
-      sbt = pkgs.sbt.overrideAttrs (
-        old: rec {
-          version = "1.3.13";
+{ java ? "openjdk11" }:
 
-          patchPhase = ''
-            echo -java-home ${pkgs.openjdk11} >> conf/sbtopts
-          '';
-        }
-      );
+let
+  nixpkgs = fetchTarball {
+    name   = "nixos-unstable-2020-09-25";
+    url    = "https://github.com/NixOS/nixpkgs-channels/archive/72b9660dc18b.tar.gz";
+    sha256 = "1cqgpw263bz261bgz34j6hiawi4hi6smwp6981yz375fx0g6kmss";
+  };
+
+  config = {
+    packageOverrides = p: {
+      sbt = p.sbt.override {
+        jre = p.${java};
+      };
     };
   };
 
-  nixpkgs = fetchTarball {
-    name   = "NixOS-unstable-08-06-2020";
-    url    = "https://github.com/NixOS/nixpkgs-channels/archive/dcb64ea42e6.tar.gz";
-    sha256 = "0i77sgs0gic6pwbkvk9lbpfshgizdrqyh18law2ji1409azc09w0";
-  };
   pkgs = import nixpkgs { inherit config; };
 in
   pkgs.mkShell {
-    buildInputs = with pkgs; [
-      jekyll # 4.1.0
-      openjdk11 # 11.0.6-internal
-      sbt # 1.3.13
+    buildInputs = [
+      pkgs.jekyll
+      pkgs.${java}
+      pkgs.sbt
     ];
   }
