51580294e1e34b75fe189f5600d58576d255096a
Michael Pilquist <mpilquist@gmail.com>
2020-10-21 08:20:33 -0400

    Fix merge

--- core/shared/src/main/scala/fs2/internal/CompileScope.scala
+++ core/shared/src/main/scala/fs2/internal/CompileScope.scala
@@ -25,7 +25,7 @@ import scala.annotation.tailrec
 
 import cats.{Id, Traverse, TraverseFilter}
 import cats.data.Chain
-import cats.effect.kernel.{Outcome, Ref, Resource}
+import cats.effect.kernel.{Outcome, Poll, Ref, Resource}
 import cats.syntax.all._
 
 import fs2.{Compiler, CompositeFailure, Scope}
