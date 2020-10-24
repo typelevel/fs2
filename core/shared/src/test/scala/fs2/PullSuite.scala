e0779125f35637cac3de3b5d33c25ae39420fe98
Fabio Labella <fabio.labella2@gmail.com>
2020-10-24 16:49:56 +0100

    Test cleanup: PullSuite

--- core/shared/src/test/scala/fs2/PullSuite.scala
+++ core/shared/src/test/scala/fs2/PullSuite.scala
@@ -29,15 +29,13 @@ class PullSuite extends Fs2Suite {
   test("early termination of uncons") {
     Ref.of[IO, Int](0).flatMap { ref =>
       Stream(1, 2, 3)
-        .onFinalize(ref.set(1))
+        .onFinalize(ref.update(_ + 1))
         .pull
         .echoChunk
         .void
         .stream
         .compile
-        .toList
-        .flatMap(_ => ref.get)
-        .map(it => assert(it == 1))
+        .drain >> ref.get.assertEquals(1)
     }
   }
 
@@ -46,9 +44,9 @@ class PullSuite extends Fs2Suite {
       val pull: Pull[Fallible, Int, Unit] = Pull.fromEither[Fallible](either)
 
       either match {
-        case Left(l) => assert(pull.stream.compile.toList == Left(l))
+        case Left(l) => assertEquals(pull.stream.compile.toList, Left(l))
         case Right(r) =>
-          assert(pull.stream.compile.toList == Right(List(r)))
+          assertEquals(pull.stream.compile.toList, Right(List(r)))
       }
     }
   }
