7fd73a56f4cfc6af3344a772ed79856af0c6231e
Fabio Labella <fabio.labella2@gmail.com>
2020-10-25 01:52:34 +0000

    Test cleanup: RetrySuite

--- core/shared/src/test/scala/fs2/StreamRetrySuite.scala
+++ core/shared/src/test/scala/fs2/StreamRetrySuite.scala
@@ -24,105 +24,107 @@ package fs2
 import scala.concurrent.duration._
 
 import cats.effect.IO
+import cats.syntax.all._
 
 class StreamRetrySuite extends Fs2Suite {
 
   case class RetryErr(msg: String = "") extends RuntimeException(msg)
 
   test("immediate success") {
-    IO.defer {
-      var attempts = 0
-      val job = IO {
-        attempts += 1
-        "success"
-      }
-      Stream.retry(job, 1.seconds, x => x, 100).compile.toList.map { r =>
-        assert(attempts == 1)
-        assert(r == List("success"))
+    Counter[IO].flatMap { attempts =>
+      val job = attempts.increment.as("success")
+
+      Stream.retry(job, 1.seconds, x => x, 100)
+        .compile
+        .lastOrError
+        .assertEquals("success") >> attempts.get.assertEquals(1L)
       }
     }
-  }
 
   test("eventual success") {
-    IO.defer {
-      var failures, successes = 0
-      val job = IO {
-        if (failures == 5) {
-          successes += 1; "success"
-        } else {
-          failures += 1; throw RetryErr()
+    (Counter[IO], Counter[IO]).tupled.flatMap {
+      case (failures, successes) =>
+        val job = failures.get.flatMap { n =>
+          if (n == 5) successes.increment.as("success")
+          else failures.increment >> IO.raiseError(RetryErr())
         }
-      }
-      Stream.retry(job, 100.millis, x => x, 100).compile.toList.map { r =>
-        assert(failures == 5)
-        assert(successes == 1)
-        assert(r == List("success"))
-      }
+
+        Stream.retry(job, 100.millis, x => x, 100)
+          .compile
+          .lastOrError
+          .assertEquals("success") >>
+        failures.get.assertEquals(5L) >>
+        successes.get.assertEquals(1L)
     }
   }
 
   test("maxRetries") {
-    IO.defer {
-      var failures = 0
-      val job = IO {
-        failures += 1
-        throw RetryErr(failures.toString)
-      }
-      Stream.retry(job, 100.millis, x => x, 5).compile.drain.attempt.map {
-        case Left(RetryErr(msg)) =>
-          assert(failures == 5)
-          assert(msg == "5")
-        case _ => fail("Expected a RetryErr")
-      }
+    Counter[IO].flatMap { failures =>
+      val job = for {
+        _ <- failures.increment
+        v <- failures.get
+        _ <- IO.raiseError[Unit](RetryErr(v.toString))
+      } yield ()
+
+      Stream.retry(job, 100.millis, x => x, 5)
+        .compile
+        .drain
+        .map(_ => fail("Expected a RetryErr"))
+        .recoverWith {
+          case RetryErr(msg) =>
+            failures.get.assertEquals(5L) >>
+            IO(msg).assertEquals("5")
+        }
     }
   }
 
   test("fatal") {
-    IO.defer {
-      var failures, successes = 0
-      val job = IO {
-        if (failures == 5) {
-          failures += 1; throw RetryErr("fatal")
-        } else if (failures > 5) {
-          successes += 1; "success"
-        } else {
-          failures += 1; throw RetryErr()
+    (Counter[IO], Counter[IO]).tupled.flatMap {
+      case (failures, successes) =>
+        val job = failures.get.flatMap { n =>
+          if (n == 5) failures.increment >> IO.raiseError(RetryErr("fatal"))
+          else if (n > 5) successes.increment.as("success")
+          else failures.increment >> IO.raiseError(RetryErr())
         }
-      }
+
       val f: Throwable => Boolean = _.getMessage != "fatal"
-      Stream.retry(job, 100.millis, x => x, 100, f).compile.drain.attempt.map {
-        case Left(RetryErr(msg)) =>
-          assert(failures == 6)
-          assert(successes == 0)
-          assert(msg == "fatal")
-        case _ => fail("Expected a RetryErr")
-      }
+
+        Stream.retry(job, 100.millis, x => x, 100, f)
+          .compile
+          .drain
+          .map(_ => fail("Expected a RetryErr"))
+          .recoverWith {
+            case RetryErr(msg) =>
+              failures.get.assertEquals(6L) >>
+              successes.get.assertEquals(0L) >>
+              IO(msg).assertEquals("fatal")
+        }
     }
   }
 
-  test("delays".flaky) {
-    val delays = scala.collection.mutable.ListBuffer.empty[Long]
-    val unit = 200
-    val maxTries = 5
-    def getDelays =
-      delays
-        .synchronized(delays.toList)
-        .sliding(2)
-        .map(s => (s.tail.head - s.head) / unit)
-        .toList
+  test("delays") {
+    IO.ref(List.empty[FiniteDuration]).flatMap { delays =>
+      IO.monotonic.flatMap { start =>
+        val unit = 200L
+        val maxTries = 5
+        val measuredDelays = delays.get.map {
+          _.sliding(2)
+            .map(s => (s.tail.head - s.head) / unit)
+            .toList
+        }
+        val job = IO.monotonic.flatMap { t =>
+          delays.update(_ :+ (t - start)) >> IO.raiseError(RetryErr())
+        }
+        val expected = List.range(1, maxTries).map(_.millis)
 
-    val job = {
-      val start = System.currentTimeMillis()
-      IO {
-        delays.synchronized(delays += System.currentTimeMillis() - start)
-        throw RetryErr()
+        Stream.retry(job, unit.millis, _ + unit.millis, maxTries)
+          .compile
+          .drain
+          .map(_ => fail("Expected a RetryErr"))
+          .recoverWith {
+            case RetryErr(_) => measuredDelays.assertEquals(expected)
+          }
       }
-    }
-
-    Stream.retry(job, unit.millis, _ + unit.millis, maxTries).compile.drain.attempt.map {
-      case Left(RetryErr(_)) =>
-        assert(getDelays == List.range(1, maxTries))
-      case _ => fail("Expected a RetryErr")
-    }
+    }.ticked
   }
 }
