2dcb7325149c4865c4c1ac2b546b72ca860b2cc8
Michael Pilquist <mpilquist@gmail.com>
2021-01-03 21:01:20 -0500

    Make CompositeFailure a case class to pick up an equals implementation

--- core/shared/src/main/scala/fs2/CompositeFailure.scala
+++ core/shared/src/main/scala/fs2/CompositeFailure.scala
@@ -24,9 +24,9 @@ package fs2
 import cats.data.NonEmptyList
 
 /** Represents multiple (>1) exceptions were thrown. */
-final class CompositeFailure(
-    val head: Throwable,
-    val tail: NonEmptyList[Throwable]
+case class CompositeFailure(
+    head: Throwable,
+    tail: NonEmptyList[Throwable]
 ) extends Throwable(
       s"Multiple exceptions were thrown (${1 + tail.size}), first ${head.getClass.getName}: ${head.getMessage}",
       head
