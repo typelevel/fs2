6e5e99485b4e982abb31e1241b8048c7cb4d6791
Fabio Labella <fabio.labella2@gmail.com>
2021-02-21 23:58:19 +0000

    Example of scoping subscriptions properly

---
+++ core/shared/src/main/scala/fs2/Ex.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2013 Functional Streams for Scala
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package fs2
+
+import cats.effect._
+import cats.effect.unsafe.implicits.global
+import scala.concurrent.duration._
+import cats.syntax.all._
+
+object Ex {
+
+  def res(name: String) =
+    Resource.make(
+      IO.println(s"opening $name")
+        .as(IO.sleep(scala.util.Random.nextInt(1000).millis) >> IO.println(s"$name executed"))
+    )(_ => IO.println(s"closing $name"))
+
+  // problem:
+  //  you want all the subscriptions opened before any subscriber starts,
+  //  but close them timely
+  // solution:
+  def a =
+    Stream
+      .range(0, 6)
+      .covary[IO]
+      .foldMap(i => Stream.resource(res(i.toString)))
+      .flatMap(
+        _.mapAsyncUnordered(100)(identity)
+      )
+      .compile
+      .drain
+      .unsafeRunSync()
+
+  def b = (Stream.resource(res("a")) ++ Stream
+    .resource(res("b"))).mapAsyncUnordered(100)(identity).compile.drain.unsafeRunSync()
+
+  def c = (Stream.resource(res("a")) ++ Stream
+    .resource(res("b"))).evalMap(identity).compile.drain.unsafeRunSync()
+
+  def d = Stream.resource {
+    (0 until 2).toList.traverse(i => res(i.toString))
+  }.flatMap(Stream.emits)
+    .mapAsyncUnordered(100)(identity)
+    .compile.drain.unsafeRunSync()
+
+  // scala> Ex.b
+  // opening a
+  // opening b
+  // a executed
+  // closing a
+  // b executed
+  // closing b
+
+  // scala> Ex.c
+  // opening a
+  // a executed
+  // closing a
+  // opening b
+  // b executed
+  // closing b
+
+  // scala> Ex.d
+  // opening 0
+  // opening 1
+  // 1 executed
+  // 0 executed
+  // closing 1
+  // closing 0
+}
