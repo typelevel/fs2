709633b8447476ae59897c7cccf76122b82dad68
Fabio Labella <fabio.labella2@gmail.com>
2021-03-07 03:41:15 +0000

    Add docs on the desired semantics for Channel

--- core/shared/src/main/scala/fs2/concurrent/Channel.scala
+++ core/shared/src/main/scala/fs2/concurrent/Channel.scala
@@ -26,13 +26,42 @@ import cats.effect._
 import cats.effect.implicits._
 import cats.syntax.all._
 
-/* Multiple producer, single consumer closeable channel */
+/*
+ * Multiple producer, single consumer closeable channel.
+ *
+ * `send` can be called concurrently by multiple producers, and it may
+ * semantically block if the Channel is bounded or synchronous.
+ *
+ * `stream` cannot be called concurrently by multiple consumers, if you do so, one of the consumers might become permanently deadlocked. It is possible to call `stream` again once the previous one has terminated, but be aware that some element might get lost in the process, e.g if the first call to stream got 5 elements off the channel, and terminated after emitting 2, when the second call to stream starts it won't see those 3 elements.
+ * the behaviour is undefined if you do so.
+ * Every time `stream` is pulled, it will serve all the elements that
+ * are queued up in a single chunk, including those from producers
+ * that might be semantically blocked on a bounded channel, which will
+ * then become unblocked.
+ *
+ * TODO is this latter decision the correct semantics? it also affects
+ * what happens during closure, i.e. whether blocked elements are
+ * processed before closing or left there. Consistency with
+ * `noneTerminated` says they should be processed in that case. it can
+ * also change the behaviour of blocked producers if you try to drain
+ * the channel as a way to unblock them, like in topic.
+ * In addition, we could have close complete the blocked producers, to avoid having to drain
+ * the channel in the Topic scenario, however it's unclear whether they should complete with
+ * Closed or Unit, because in the general we don't know if a subsequent call to `stream` is going to arrive, and we ideally want to only return Closed if the elements are discarded
+ *
+ */
+
 trait Channel[F[_], A] {
-  def send(a: A): F[Unit]
-  def close: F[Unit]
+  def send(a: A): F[Either[Channel.Closed, Unit]]
+  def close: F[Either[Channel.Closed, Unit]]
   def stream: Stream[F, A]
 }
 object Channel {
+  type Closed = Closed.type
+  object Closed
+  private final val closed: Either[Closed, Unit] = Left(Closed)
+  private final val open: Either[Closed, Unit] = Right(())
+
   def create[F[_], A](implicit F: Concurrent[F]): F[Channel[F, A]] = {
     case class State(
       values: Vector[A],
@@ -42,21 +71,21 @@ object Channel {
 
     F.ref(State(Vector.empty, None, false)).map { state =>
       new Channel[F, A] {
-        def send(a: A): F[Unit] =
+        def send(a: A) =
           state.modify {
             case s @ State(_, _, closed) if closed == true =>
-              s -> F.unit
+              s -> Channel.closed.pure[F]
             case State(values, wait, closed) =>
-              State(values :+ a, None, closed) -> wait.traverse_(_.complete(()))
+              State(values :+ a, None, closed) -> wait.traverse_(_.complete(())).as(Channel.open)
           }.flatten
             .uncancelable
 
-        def close: F[Unit] =
+        def close =
           state.modify {
             case s @ State(_, _, closed) if closed == true =>
-              s -> F.unit
+              s -> Channel.closed.pure[F]
             case State(values, wait, _) =>
-              State(values, None, true) -> wait.traverse_(_.complete(()))
+              State(values, None, true) -> wait.traverse_(_.complete(())).as(Channel.open)
           }.flatten.uncancelable
 
         def consume : Pull[F, A, Unit]  =
@@ -64,11 +93,14 @@ object Channel {
             F.deferred[Unit].flatMap { wait =>
               state.modify {
                 case State(values, _, closed) =>
-                  if (values.nonEmpty) {
+                  if (values.nonEmpty) { // values.nonEmpty || prods.nonEmpty
                     val newSt = State(Vector(), None, closed)
                     val emit = Pull.output(Chunk.vector(values))
                     val action =
                       emit >> consume.unlessA(closed)
+                    // the unlessA could be removed here,
+                    // the important part is to not wait after closure
+                    // i.e. the case below
 
                     newSt -> action
                   } else {
