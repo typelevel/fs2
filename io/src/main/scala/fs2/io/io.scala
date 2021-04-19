bd92122195c5fcc6e51bbcbabea975d0b40d7666
Vasil Vasilev <vasil@vasilev.io>
2021-04-19 03:00:40 +0200

    Use a sliced chunk instead of slicing an array

--- io/src/main/scala/fs2/io/io.scala
+++ io/src/main/scala/fs2/io/io.scala
@@ -74,7 +74,7 @@ package object io {
     F.blocking(is.read(buf)).map { numBytes =>
       if (numBytes < 0) None
       else if (numBytes == 0) Some(Chunk.empty)
-      else if (numBytes < buf.size) Some(Chunk.array(buf.slice(0, numBytes)))
+      else if (numBytes < buf.size) Some(Chunk.array(buf, 0, numBytes))
       else Some(Chunk.array(buf))
     }
 
