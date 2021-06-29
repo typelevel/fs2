4abf0b7498fceda8b8275e8276e32efbd53373a2
Arman Bilge <armanbilge@gmail.com>
2021-06-29 02:46:34 +0000

    Implement FileHandle for JS

--- io/js/src/main/scala/fs2/io/file/FileHandlePlatform.scala
+++ io/js/src/main/scala/fs2/io/file/FileHandlePlatform.scala
@@ -23,4 +23,53 @@ package fs2
 package io
 package file
 
-private[file] trait FileHandlePlatform
+import typings.node.fsPromisesMod
+import cats.effect.kernel.Async
+import cats.syntax.all._
+import scala.scalajs.js.typedarray.ArrayBuffer
+import scala.scalajs.js.typedarray.TypedArrayBuffer
+import scala.scalajs.js.typedarray.TypedArrayBufferOps._
+
+private[file] trait FileHandlePlatform {
+  private[file] def make[F[_]](
+      fd: fsPromisesMod.FileHandle
+  )(implicit F: Async[F]): FileHandle[F] =
+    new FileHandle[F] {
+      type Lock = Unit
+
+      override def force(metaData: Boolean): F[Unit] =
+        F.fromPromise(F.delay(fsPromisesMod.fdatasync(fd)))
+
+      override def lock: F[Lock] =
+        F.raiseError(new UnsupportedOperationException)
+
+      override def lock(position: Long, size: Long, shared: Boolean): F[Lock] =
+        F.raiseError(new UnsupportedOperationException)
+
+      override def read(numBytes: Int, offset: Long): F[Option[Chunk[Byte]]] =
+        F.fromPromise(F.delay(fd.read(new ArrayBuffer(numBytes), offset.toDouble, numBytes))).map { res =>
+          if (res.bytesRead < 0) None
+          else if (res.bytesRead == 0) Some(Chunk.empty)
+          else Some(Chunk.byteBuffer(TypedArrayBuffer.wrap(res.buffer).limit(res.bytesRead.toInt)))
+        }
+
+      override def size: F[Long] =
+        F.fromPromise(F.delay(fd.stat())).map(_.size.toLong)
+
+      override def truncate(size: Long): F[Unit] =
+        F.fromPromise(F.delay(fd.truncate(size.toDouble)))
+
+      override def tryLock: F[Option[Lock]] =
+        F.raiseError(new UnsupportedOperationException)
+
+      override def tryLock(position: Long, size: Long, shared: Boolean): F[Option[Lock]] =
+        F.raiseError(new UnsupportedOperationException)
+
+      override def unlock(f: Lock): F[Unit] =
+        F.raiseError(new UnsupportedOperationException)
+
+      override def write(bytes: Chunk[Byte], offset: Long): F[Int] =
+        F.fromPromise(F.delay(fd.write(bytes.toByteBuffer.arrayBuffer(), offset.toDouble)))
+          .map(_.bytesWritten.toInt)
+    }
+}
