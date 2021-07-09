7963b559b811e45ed6aa22fd6a239a7f0f8362e9
Michael Pilquist <mpilquist@gmail.com>
2021-07-09 09:17:31 -0400

    Groundwork for text.hex.encode/decode

--- core/shared/src/main/scala/fs2/text.scala
+++ core/shared/src/main/scala/fs2/text.scala
@@ -29,144 +29,165 @@ import scodec.bits.{Bases, ByteVector}
 
 /** Provides utilities for working with streams of text (e.g., encoding byte streams to strings). */
 object text {
-  private val utf8Charset = Charset.forName("UTF-8")
-  private[this] val utf8BomSeq: Seq[Byte] = Array(0xef.toByte, 0xbb.toByte, 0xbf.toByte).toSeq
+
+  object utf8 {
+    private val utf8Charset = Charset.forName("UTF-8")
+    private[this] val utf8BomSeq: Seq[Byte] = Array(0xef.toByte, 0xbb.toByte, 0xbf.toByte).toSeq
+
+    /** Converts UTF-8 encoded byte stream to a stream of `String`. */
+    def decode[F[_]]: Pipe[F, Byte, String] =
+      _.chunks.through(decodeC)
+
+    /** Converts UTF-8 encoded `Chunk[Byte]` inputs to `String`. */
+    def decodeC[F[_]]: Pipe[F, Chunk[Byte], String] = {
+      /*
+      * Returns the number of continuation bytes if `b` is an ASCII byte or a
+      * leading byte of a multi-byte sequence, and -1 otherwise.
+      */
+      def continuationBytes(b: Byte): Int =
+        if ((b & 0x80) == 0x00) 0 // ASCII byte
+        else if ((b & 0xe0) == 0xc0) 1 // leading byte of a 2 byte seq
+        else if ((b & 0xf0) == 0xe0) 2 // leading byte of a 3 byte seq
+        else if ((b & 0xf8) == 0xf0) 3 // leading byte of a 4 byte seq
+        else -1 // continuation byte or garbage
+
+      /*
+      * Returns the length of an incomplete multi-byte sequence at the end of
+      * `bs`. If `bs` ends with an ASCII byte or a complete multi-byte sequence,
+      * 0 is returned.
+      */
+      def lastIncompleteBytes(bs: Array[Byte]): Int = {
+        /*
+        * This is logically the same as this
+        * code, but written in a low level way
+        * to avoid any allocations and just do array
+        * access
+        *
+        *
+        *
+          val lastThree = bs.drop(0.max(bs.size - 3)).toArray.reverseIterator
+          lastThree
+            .map(continuationBytes)
+            .zipWithIndex
+            .find {
+              case (c, _) => c >= 0
+            }
+            .map {
+              case (c, i) => if (c == i) 0 else i + 1
+            }
+            .getOrElse(0)
+
+        */
+
+        val minIdx = 0.max(bs.length - 3)
+        var idx = bs.length - 1
+        var counter = 0
+        var res = 0
+        while (minIdx <= idx) {
+          val c = continuationBytes(bs(idx))
+          if (c >= 0) {
+            if (c != counter)
+              res = counter + 1
+            // exit the loop
+            return res
+          }
+          idx = idx - 1
+          counter = counter + 1
+        }
+        res
+      }
+
+      def processSingleChunk(
+          bldr: Builder[String, List[String]],
+          buffer: Chunk[Byte],
+          nextBytes: Chunk[Byte]
+      ): Chunk[Byte] = {
+        // if processing ASCII or largely ASCII buffer is often empty
+        val allBytes =
+          if (buffer.isEmpty) nextBytes.toArray
+          else Array.concat(buffer.toArray, nextBytes.toArray)
+
+        val splitAt = allBytes.length - lastIncompleteBytes(allBytes)
+
+        if (splitAt == allBytes.length) {
+          // in the common case of ASCII chars
+          // we are in this branch so the next buffer will
+          // be empty
+          bldr += new String(allBytes, utf8Charset)
+          Chunk.empty
+        } else if (splitAt == 0)
+          Chunk.array(allBytes)
+        else {
+          bldr += new String(allBytes.take(splitAt), utf8Charset)
+          Chunk.array(allBytes.drop(splitAt))
+        }
+      }
+
+      def doPull(buf: Chunk[Byte], s: Stream[F, Chunk[Byte]]): Pull[F, String, Unit] =
+        s.pull.uncons.flatMap {
+          case Some((byteChunks, tail)) =>
+            // use local and private mutability here
+            var idx = 0
+            val size = byteChunks.size
+            val bldr = List.newBuilder[String]
+            var buf1 = buf
+            while (idx < size) {
+              val nextBytes = byteChunks(idx)
+              buf1 = processSingleChunk(bldr, buf1, nextBytes)
+              idx = idx + 1
+            }
+            Pull.output(Chunk.seq(bldr.result())) >> doPull(buf1, tail)
+          case None if buf.nonEmpty =>
+            Pull.output1(new String(buf.toArray, utf8Charset))
+          case None =>
+            Pull.done
+        }
+
+      def processByteOrderMark(
+          buffer: Chunk.Queue[Byte] /* or null which we use as an Optional type to avoid boxing */,
+          s: Stream[F, Chunk[Byte]]
+      ): Pull[F, String, Unit] =
+        s.pull.uncons1.flatMap {
+          case Some((hd, tl)) =>
+            val newBuffer0 =
+              if (buffer ne null) buffer
+              else Chunk.Queue.empty[Byte]
+
+            val newBuffer: Chunk.Queue[Byte] = newBuffer0 :+ hd
+            if (newBuffer.size >= 3) {
+              val rem =
+                if (newBuffer.startsWith(utf8BomSeq)) newBuffer.drop(3)
+                else newBuffer
+              doPull(Chunk.empty, Stream.emits(rem.chunks) ++ tl)
+            } else
+              processByteOrderMark(newBuffer, tl)
+          case None =>
+            if (buffer ne null)
+              doPull(Chunk.empty, Stream.emits(buffer.chunks))
+            else Pull.done
+        }
+
+      (in: Stream[F, Chunk[Byte]]) => processByteOrderMark(null, in).stream
+    }
+
+    /** Encodes a stream of `String` in to a stream of bytes using the UTF-8 charset. */
+    def encode[F[_]]: Pipe[F, String, Byte] =
+      text.encode(utf8Charset)
+
+    /** Encodes a stream of `String` in to a stream of `Chunk[Byte]` using the UTF-8 charset. */
+    def encodeC[F[_]]: Pipe[F, String, Chunk[Byte]] =
+      text.encodeC(utf8Charset)
+  }
 
   /** Converts UTF-8 encoded byte stream to a stream of `String`. */
+  @deprecated("Use text.utf8.decode", "3.0.7")
   def utf8Decode[F[_]]: Pipe[F, Byte, String] =
-    _.chunks.through(utf8DecodeC)
+    utf8.decode
 
   /** Converts UTF-8 encoded `Chunk[Byte]` inputs to `String`. */
-  def utf8DecodeC[F[_]]: Pipe[F, Chunk[Byte], String] = {
-    /*
-     * Returns the number of continuation bytes if `b` is an ASCII byte or a
-     * leading byte of a multi-byte sequence, and -1 otherwise.
-     */
-    def continuationBytes(b: Byte): Int =
-      if ((b & 0x80) == 0x00) 0 // ASCII byte
-      else if ((b & 0xe0) == 0xc0) 1 // leading byte of a 2 byte seq
-      else if ((b & 0xf0) == 0xe0) 2 // leading byte of a 3 byte seq
-      else if ((b & 0xf8) == 0xf0) 3 // leading byte of a 4 byte seq
-      else -1 // continuation byte or garbage
-
-    /*
-     * Returns the length of an incomplete multi-byte sequence at the end of
-     * `bs`. If `bs` ends with an ASCII byte or a complete multi-byte sequence,
-     * 0 is returned.
-     */
-    def lastIncompleteBytes(bs: Array[Byte]): Int = {
-      /*
-       * This is logically the same as this
-       * code, but written in a low level way
-       * to avoid any allocations and just do array
-       * access
-       *
-       *
-       *
-        val lastThree = bs.drop(0.max(bs.size - 3)).toArray.reverseIterator
-        lastThree
-          .map(continuationBytes)
-          .zipWithIndex
-          .find {
-            case (c, _) => c >= 0
-          }
-          .map {
-            case (c, i) => if (c == i) 0 else i + 1
-          }
-          .getOrElse(0)
-
-       */
-
-      val minIdx = 0.max(bs.length - 3)
-      var idx = bs.length - 1
-      var counter = 0
-      var res = 0
-      while (minIdx <= idx) {
-        val c = continuationBytes(bs(idx))
-        if (c >= 0) {
-          if (c != counter)
-            res = counter + 1
-          // exit the loop
-          return res
-        }
-        idx = idx - 1
-        counter = counter + 1
-      }
-      res
-    }
-
-    def processSingleChunk(
-        bldr: Builder[String, List[String]],
-        buffer: Chunk[Byte],
-        nextBytes: Chunk[Byte]
-    ): Chunk[Byte] = {
-      // if processing ASCII or largely ASCII buffer is often empty
-      val allBytes =
-        if (buffer.isEmpty) nextBytes.toArray
-        else Array.concat(buffer.toArray, nextBytes.toArray)
-
-      val splitAt = allBytes.length - lastIncompleteBytes(allBytes)
-
-      if (splitAt == allBytes.length) {
-        // in the common case of ASCII chars
-        // we are in this branch so the next buffer will
-        // be empty
-        bldr += new String(allBytes, utf8Charset)
-        Chunk.empty
-      } else if (splitAt == 0)
-        Chunk.array(allBytes)
-      else {
-        bldr += new String(allBytes.take(splitAt), utf8Charset)
-        Chunk.array(allBytes.drop(splitAt))
-      }
-    }
-
-    def doPull(buf: Chunk[Byte], s: Stream[F, Chunk[Byte]]): Pull[F, String, Unit] =
-      s.pull.uncons.flatMap {
-        case Some((byteChunks, tail)) =>
-          // use local and private mutability here
-          var idx = 0
-          val size = byteChunks.size
-          val bldr = List.newBuilder[String]
-          var buf1 = buf
-          while (idx < size) {
-            val nextBytes = byteChunks(idx)
-            buf1 = processSingleChunk(bldr, buf1, nextBytes)
-            idx = idx + 1
-          }
-          Pull.output(Chunk.seq(bldr.result())) >> doPull(buf1, tail)
-        case None if buf.nonEmpty =>
-          Pull.output1(new String(buf.toArray, utf8Charset))
-        case None =>
-          Pull.done
-      }
-
-    def processByteOrderMark(
-        buffer: Chunk.Queue[Byte] /* or null which we use as an Optional type to avoid boxing */,
-        s: Stream[F, Chunk[Byte]]
-    ): Pull[F, String, Unit] =
-      s.pull.uncons1.flatMap {
-        case Some((hd, tl)) =>
-          val newBuffer0 =
-            if (buffer ne null) buffer
-            else Chunk.Queue.empty[Byte]
-
-          val newBuffer: Chunk.Queue[Byte] = newBuffer0 :+ hd
-          if (newBuffer.size >= 3) {
-            val rem =
-              if (newBuffer.startsWith(utf8BomSeq)) newBuffer.drop(3)
-              else newBuffer
-            doPull(Chunk.empty, Stream.emits(rem.chunks) ++ tl)
-          } else
-            processByteOrderMark(newBuffer, tl)
-        case None =>
-          if (buffer ne null)
-            doPull(Chunk.empty, Stream.emits(buffer.chunks))
-          else Pull.done
-      }
-
-    (in: Stream[F, Chunk[Byte]]) => processByteOrderMark(null, in).stream
-  }
+  @deprecated("Use text.utf8.decodeC", "3.0.7")
+  def utf8DecodeC[F[_]]: Pipe[F, Chunk[Byte], String] =
+    utf8.decodeC
 
   /** Encodes a stream of `String` in to a stream of bytes using the given charset. */
   def encode[F[_]](charset: Charset): Pipe[F, String, Byte] =
@@ -177,12 +198,14 @@ object text {
     _.mapChunks(_.map(s => Chunk.array(s.getBytes(charset))))
 
   /** Encodes a stream of `String` in to a stream of bytes using the UTF-8 charset. */
+  @deprecated("Use text.utf8.encode", "3.0.7")
   def utf8Encode[F[_]]: Pipe[F, String, Byte] =
-    encode(utf8Charset)
+    utf8.encode
 
   /** Encodes a stream of `String` in to a stream of `Chunk[Byte]` using the UTF-8 charset. */
+  @deprecated("Use text.utf8.encodeC", "3.0.7")
   def utf8EncodeC[F[_]]: Pipe[F, String, Chunk[Byte]] =
-    encodeC(utf8Charset)
+    utf8.encodeC
 
   /** Transforms a stream of `String` such that each emitted `String` is a line from the input. */
   def lines[F[_]]: Pipe[F, String, String] = {
@@ -440,5 +463,43 @@ object text {
 
       in => go(ByteVector.empty, in).stream
     }
+  }
+
+  /** Functions for working with base 64. */
+  object hex {
+
+    /** Converts a stream of hex text in to a stream of bytes.
+      *
+      * If the text is not valid hex, the pipe fails with an exception.
+      * There must be an even number of hex digits (nibbles) or the pipe
+      * fails with an exception.  Whitespace characters are ignored.
+      *
+      * The default alphabet is used by this pipe.
+      */
+    def decode[F[_]: RaiseThrowable]: Pipe[F, String, Byte] =
+      decodeWithAlphabet(Bases.Alphabets.HexLowercase)
+
+    def decodeWithAlphabet[F[_]: RaiseThrowable](alphabet: Bases.HexAlphabet): Pipe[F, String, Byte] = {
+      def go(s: Stream[F, String], carry: String): Pull[F, Byte, Unit] =
+        s.pull.uncons1.flatMap {
+          case Some((hd, tl)) =>
+            val s = carry ++ hd
+            // TODO this is wrong as we need to account for whitespace when computing carry
+            val (toParse, newCarry) = if (s.size % 2 == 0) (s, "") else (s.init, s.last.toString)
+            ByteVector.fromHexDescriptive(toParse, alphabet) match {
+              case Left(err) => Pull.raiseError(new IllegalArgumentException(err))
+              case Right(out) => Pull.output(Chunk.byteVector(out)) >> go(tl, newCarry)
+            }
+          case None =>
+            if (carry.isEmpty) Pull.done else Pull.raiseError(new IllegalArgumentException("Nibble left over"))
+        }
+      s => go(s, "").stream
+    }
+
+    def encode[F[_]]: Pipe[F, Byte, String] =
+      encodeWithAlphabet(Bases.Alphabets.HexLowercase)
+
+    def encodeWithAlphabet[F[_]](alphabet: Bases.HexAlphabet): Pipe[F, Byte, String] =
+      _.chunks.map(c => c.toByteVector.toHex(alphabet))
   }
 }
