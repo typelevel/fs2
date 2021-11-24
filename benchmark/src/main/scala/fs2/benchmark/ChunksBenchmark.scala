1167bbbad9407e7ea2900e7d8cc9d2570bac8189
Michael Pilquist <mpilquist@gmail.com>
2021-11-23 21:06:32 -0500

    Improve performance of Chunk.map and similar and memory efficiency of filter

--- benchmark/src/main/scala/fs2/benchmark/ChunksBenchmark.scala
+++ benchmark/src/main/scala/fs2/benchmark/ChunksBenchmark.scala
@@ -24,27 +24,46 @@ package benchmark
 
 import org.openjdk.jmh.annotations.{Benchmark, Param, Scope, Setup, State}
 
+import cats.syntax.all._
+
 @State(Scope.Thread)
-class ChunkBenchmark {
+class ChunksBenchmark {
   @Param(Array("16", "256", "4096"))
   var chunkSize: Int = _
 
-  var ints: Chunk[Int] = _
+  @Param(Array("1", "20", "50", "100"))
+  var chunkCount: Int = _
+
+  case class Obj(dummy: Boolean)
+  object Obj {
+    def create: Obj = Obj(true)
+  }
+
+  var chunkSeq: Seq[Chunk[Obj]] = _
+  var flattened: Chunk[Obj] = _
+  var sizeHint: Int = _
 
   @Setup
   def setup() = {
-    ints = Chunk.seq((0 until chunkSize).map(_ + 1000)).compact
+    chunkSeq = Seq.range(0, chunkCount).map(_ => Chunk.seq(Seq.fill(chunkSize)(Obj.create)))
+    sizeHint = chunkSeq.foldLeft(0)(_ + _.size)
+    flattened = Chunk.seq(chunkSeq).flatten
   }
 
   @Benchmark
-  def map(): Unit = {
-    ints.map(_ + 1)
+  def concat(): Unit = {
+    Chunk.concat(chunkSeq, sizeHint)
     ()
   }
 
   @Benchmark
-  def filter(): Unit = {
-    ints.filter(_ % 3 == 0)
-    ()
+  def traverse(): Boolean = {
+    val fn: () => Chunk[Boolean] =
+      flattened.traverse { obj =>
+        if (obj.dummy) { () => true }
+        else { () => false }
+      }
+
+    fn().isEmpty
   }
 }
