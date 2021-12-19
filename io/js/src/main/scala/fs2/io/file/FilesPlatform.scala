f0d4272b93d1a0322ea5f49f1fb12e33a6ab3ec3
Iurii Malchenko <i@yurique.com>
2021-12-19 10:03:41 +0200

    fsIO - realPath:  re-throw javascript exception as NoSuchFileException

--- io/js/src/main/scala/fs2/io/file/FilesPlatform.scala
+++ io/js/src/main/scala/fs2/io/file/FilesPlatform.scala
@@ -321,7 +321,10 @@ private[fs2] trait FilesCompanionPlatform {
       readStream(path, chunkSize, Flags.Read)(_.setStart(start.toDouble).setEnd((end - 1).toDouble))
 
     def realPath(path: Path): F[Path] =
-      F.fromPromise(F.delay(fsPromisesMod.realpath(path.toString))).map(Path(_))
+      F.fromPromise(F.delay(fsPromisesMod.realpath(path.toString))).map(Path(_)).handleErrorWith {
+        case NoSuchFileException(e) =>
+          F.raiseError(e)
+      }
 
     override def setFileTimes(
         path: Path,
