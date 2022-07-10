79344baae70b6e3d2a3731f5e7f8e3d81ce9eedb
Arman Bilge <armanbilge@gmail.com>
2022-07-10 20:06:21 +0000

    Formatting

--- core/shared/src/main/scala/fs2/Chunk.scala
+++ core/shared/src/main/scala/fs2/Chunk.scala
@@ -365,7 +365,7 @@ abstract class Chunk[+O] extends Serializable with ChunkPlatform[O] with ChunkRu
   def toByteVector[B >: O](implicit ev: B =:= Byte): ByteVector =
     this match {
       case c: Chunk.ByteVectorChunk => c.toByteVector
-      case c: Chunk.ByteBuffer =>
+      case c: Chunk.ByteBuffer      =>
         // if c is a view, position/limit of underlying buffer may have changed
         val bb = c.buf.asReadOnlyBuffer()
         bb.position(c.offset)
