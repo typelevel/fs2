b5b3a049ed64397ae5fca02a0f48d93c231cd64b
Arman Bilge <armanbilge@gmail.com>
2022-12-22 07:55:20 +0000

    Expose new polling system `Network`

--- io/native/src/main/scala/fs2/io/net/NetworkPlatform.scala
+++ io/native/src/main/scala/fs2/io/net/NetworkPlatform.scala
@@ -23,9 +23,10 @@ package fs2
 package io
 package net
 
+import cats.effect.LiftIO
 import cats.effect.kernel.{Async, Resource}
 
-import com.comcast.ip4s.{Host, IpAddress, Port, SocketAddress}
+import com.comcast.ip4s.{Dns, Host, IpAddress, Port, SocketAddress}
 
 import fs2.io.net.tls.TLSContext
 
@@ -33,9 +34,9 @@ private[net] trait NetworkPlatform[F[_]]
 
 private[net] trait NetworkCompanionPlatform { self: Network.type =>
 
-  implicit def forAsync[F[_]](implicit F: Async[F]): Network[F] =
+  implicit def forAsync[F[_]: Async: Dns: LiftIO]: Network[F] =
     new UnsealedNetwork[F] {
-      private lazy val globalSocketGroup = SocketGroup.unsafe[F](null)
+      private lazy val globalSocketGroup = new FdPollingSocketGroup[F]
 
       def client(
           to: SocketAddress[Host],
@@ -56,6 +57,32 @@ private[net] trait NetworkCompanionPlatform { self: Network.type =>
         globalSocketGroup.serverResource(address, port, options)
 
       def tlsContext: TLSContext.Builder[F] = TLSContext.Builder.forAsync
+    }
+
+  @deprecated("Prefer the IO polling system-based implementation", "3.5.0")
+  def forAsync[F[_]](F: Async[F]): Network[F] =
+    new UnsealedNetwork[F] {
+      private lazy val globalSocketGroup = SocketGroup.unsafe[F](null)(F)
+
+      def client(
+          to: SocketAddress[Host],
+          options: List[SocketOption]
+      ): Resource[F, Socket[F]] = globalSocketGroup.client(to, options)
+
+      def server(
+          address: Option[Host],
+          port: Option[Port],
+          options: List[SocketOption]
+      ): Stream[F, Socket[F]] = globalSocketGroup.server(address, port, options)
+
+      def serverResource(
+          address: Option[Host],
+          port: Option[Port],
+          options: List[SocketOption]
+      ): Resource[F, (SocketAddress[IpAddress], Stream[F, Socket[F]])] =
+        globalSocketGroup.serverResource(address, port, options)
+
+      def tlsContext: TLSContext.Builder[F] = TLSContext.Builder.forAsync(F)
     }
 
 }
