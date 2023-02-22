c3da68ce3b90696fb24f530e477d9476709893ed
Arman Bilge <armanbilge@gmail.com>
2023-02-22 08:19:48 +0000

    Add a bunch of tests

--- io/shared/src/test/scala/fs2/io/process/ProcessSuite.scala
+++ io/shared/src/test/scala/fs2/io/process/ProcessSuite.scala
@@ -23,9 +23,8 @@ package fs2
 package io
 package process
 
-import cats.effect.{IO, Resource}
-import cats.kernel.Order
-import cats.syntax.all._
+import cats.effect.IO
+import fs2.io.file.Files
 
 import scala.concurrent.duration._
 
@@ -40,6 +39,76 @@ class ProcessSuite extends Fs2IoSuite {
           .string
           .assertEquals("hello world\n") *> p.exitValue.assertEquals(0)
     }
+  }
+
+  test("stdout and stderr") {
+    ProcessSpawn[IO]
+      .spawn(
+        ProcessBuilder(
+          "node",
+          List("-e", "console.log('good day stdout'); console.error('how do you do stderr')")
+        )
+      )
+      .use { p =>
+        val testOut = p.stdout
+          .through(fs2.text.utf8.decode)
+          .compile
+          .string
+          .assertEquals("good day stdout\n")
+
+        val testErr = p.stderr
+          .through(fs2.text.utf8.decode)
+          .compile
+          .string
+          .assertEquals("how do you do stderr\n")
+
+        val textExit = p.exitValue.assertEquals(0)
+
+        testOut.both(testErr).both(textExit).void
+      }
+  }
+
+  test("cat") {
+    ProcessSpawn[IO].spawn(ProcessBuilder("cat", Nil)).use { p =>
+      val verySpecialMsg = "FS2 rocks!"
+      val in = Stream.emit(verySpecialMsg).through(fs2.text.utf8.encode).through(p.stdin)
+      val out = p.stdout.through(fs2.text.utf8.decode)
+
+      out
+        .concurrently(in)
+        .compile
+        .string
+        .assertEquals(verySpecialMsg)
+    }
+  }
+
+  test("working directory") {
+    Files[IO].tempDirectory.use { wd =>
+      ProcessSpawn[IO].spawn(ProcessBuilder("pwd", Nil).withWorkingDirectory(wd)).use { p =>
+        p.stdout.through(fs2.text.utf8.decode).compile.string.assertEquals(wd.toString + "\n")
+      }
+    }
+  }
+
+  test("env") {
+    ProcessSpawn[IO].spawn(ProcessBuilder("env", Nil).withEnv(Map("FS2" -> "ROCKS"))).use { p =>
+      p.stdout
+        .through(fs2.text.utf8.decode)
+        .through(fs2.text.lines)
+        .exists(_ == "FS2=ROCKS")
+        .compile
+        .onlyOrError
+        .assert
+    }
+  }
+
+  test("cancelation") {
+    ProcessSpawn[IO]
+      .spawn(ProcessBuilder("cat", Nil))
+      .use { p =>
+        p.stdout.compile.drain.both(p.stderr.compile.drain).both(p.exitValue).void
+      }
+      .timeoutTo(1.second, IO.unit) // assert that cancelation does not hang
   }
 
 }
