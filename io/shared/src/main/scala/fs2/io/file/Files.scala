8325becb9cc03b5fd42e5332dcd54ad126884b20
Antonio Gelameris <toniogela89@gmail.com>
2023-03-19 10:17:42 +0100

    Moved implementations in to trait

--- io/shared/src/main/scala/fs2/io/file/Files.scala
+++ io/shared/src/main/scala/fs2/io/file/Files.scala
@@ -269,7 +269,10 @@ sealed trait Files[F[_]] extends FilesPlatform[F] {
   def readAll(path: Path, chunkSize: Int, flags: Flags): Stream[F, Byte]
 
   /** Returns a `ReadCursor` for the specified path, using the supplied flags when opening the file. */
-  def readCursor(path: Path, flags: Flags): Resource[F, ReadCursor[F]]
+  def readCursor(path: Path, flags: Flags): Resource[F, ReadCursor[F]] =
+    open(path, flags.addIfAbsent(Flag.Read)).map { fileHandle =>
+      ReadCursor(fileHandle, 0L)
+    }
 
   /** Reads a range of data synchronously from the file at the specified path.
     * `start` is inclusive, `end` is exclusive, so when `start` is 0 and `end` is 2,
@@ -278,10 +281,10 @@ sealed trait Files[F[_]] extends FilesPlatform[F] {
   def readRange(path: Path, chunkSize: Int, start: Long, end: Long): Stream[F, Byte]
 
   /** Reads all bytes from the file specified and decodes them as a utf8 string. */
-  def readUtf8(path: Path): Stream[F, String]
+  def readUtf8(path: Path): Stream[F, String] = readAll(path).through(text.utf8.decode)
 
   /** Reads all bytes from the file specified and decodes them as utf8 lines. */
-  def readUtf8Lines(path: Path): Stream[F, String]
+  def readUtf8Lines(path: Path): Stream[F, String] = readUtf8(path).through(text.lines)
 
   /** Returns the real path i.e. the actual location of `path`.
     * The precise definition of this method is implementation dependent but in general
@@ -421,7 +424,8 @@ sealed trait Files[F[_]] extends FilesPlatform[F] {
   /** Writes to the specified file as an utf8 string using
     * the specified flags to open the file.
     */
-  def writeUtf8(path: Path, flags: Flags): Pipe[F, String, Nothing]
+  def writeUtf8(path: Path, flags: Flags): Pipe[F, String, Nothing] = in =>
+    in.through(text.utf8.encode).through(writeAll(path, flags))
 
   /** Writes each string to the specified file as utf8 lines.
     *
@@ -434,7 +438,8 @@ sealed trait Files[F[_]] extends FilesPlatform[F] {
   /** Writes each string to the specified file as utf8 lines
     * using the specified flags to open the file.
     */
-  def writeUtf8Lines(path: Path, flags: Flags): Pipe[F, String, Nothing]
+  def writeUtf8Lines(path: Path, flags: Flags): Pipe[F, String, Nothing] = in =>
+    in.through(text.lines).through(writeUtf8(path, flags))
 }
 
 object Files extends FilesCompanionPlatform {
@@ -445,21 +450,10 @@ object Files extends FilesCompanionPlatform {
         cursor.readAll(chunkSize).void.stream
       }
 
-    def readCursor(path: Path, flags: Flags): Resource[F, ReadCursor[F]] =
-      open(path, flags.addIfAbsent(Flag.Read)).map { fileHandle =>
-        ReadCursor(fileHandle, 0L)
-      }
-
     def readRange(path: Path, chunkSize: Int, start: Long, end: Long): Stream[F, Byte] =
       Stream.resource(readCursor(path, Flags.Read)).flatMap { cursor =>
         cursor.seek(start).readUntil(chunkSize, end).void.stream
       }
-
-    def readUtf8(path: Path): Stream[F, String] =
-      readAll(path).through(text.utf8.decode)
-
-    def readUtf8Lines(path: Path): Stream[F, String] =
-      readUtf8(path).through(text.lines)
 
     def tail(
         path: Path,
@@ -599,12 +593,6 @@ object Files extends FilesCompanionPlatform {
             }
           }
     }
-
-    def writeUtf8(path: Path, flags: Flags): Pipe[F, String, Nothing] =
-      _.through(text.utf8.encode).through(writeAll(path, flags))
-
-    def writeUtf8Lines(path: Path, flags: Flags): Pipe[F, String, Nothing] =
-      _.through(text.lines).through(writeUtf8(path, flags))
   }
 
   def apply[F[_]](implicit F: Files[F]): Files[F] = F
