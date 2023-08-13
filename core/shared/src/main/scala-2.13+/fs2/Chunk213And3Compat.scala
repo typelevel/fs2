7729762f9a9f84c1b962f1f22ee2471316c70ee0
Luis Miguel Mejía Suárez <luismiguelmejiasuarez@gmail.com>
2023-08-13 17:53:44 -0500

    Override more operations in ChunkAsSeq

--- core/shared/src/main/scala-2.13+/fs2/Chunk213And3Compat.scala
+++ core/shared/src/main/scala-2.13+/fs2/Chunk213And3Compat.scala
@@ -21,8 +21,8 @@
 
 package fs2
 
+import scala.collection.Factory
 import scala.collection.immutable.ArraySeq
-import scala.collection.immutable
 import scala.reflect.ClassTag
 
 private[fs2] trait Chunk213And3Compat[+O] { self: Chunk[O] =>
@@ -51,9 +51,7 @@ private[fs2] trait Chunk213And3Compat[+O] { self: Chunk[O] =>
     new ChunkAsSeq(this)
 }
 
-private[fs2] final class ChunkAsSeq[+O](chunk: Chunk[O])
-    extends immutable.Seq[O]
-    with Serializable {
+private[fs2] final class ChunkAsSeq[+O](chunk: Chunk[O]) extends Seq[O] with Serializable {
   override def iterator: Iterator[O] =
     chunk.iterator
 
@@ -62,23 +60,77 @@ private[fs2] final class ChunkAsSeq[+O](chunk: Chunk[O])
 
   override def length: Int =
     chunk.size
+
+  override def foreach[U](f: O => U): Unit =
+    chunk.foreach { o => f(o); () }
+
+  override def tapEach[U](f: O => U): Seq[O] = {
+    chunk.foreach { o => f(o); () }
+    this
+  }
+
+  override def isEmpty: Boolean =
+    chunk.isEmpty
+
+  override def copyToArray[B >: O](xs: Array[B], start: Int, len: Int): Int = {
+    chunk.take(len).copyToArray(xs, start)
+    math.min(len, xs.length - start)
+  }
+
+  override def headOption: Option[O] =
+    chunk.head
+
+  override def head: O =
+    if (chunk.nonEmpty) chunk.apply(0)
+    else throw new NoSuchElementException("head of empty Seq")
+
+  override def lastOption: Option[O] =
+    chunk.last
+
+  override def last: O =
+    if (chunk.nonEmpty) chunk.apply(chunk.size - 1)
+    else throw new NoSuchElementException("tail of empty Seq")
+
+  override def take(n: Int): Seq[O] =
+    new ChunkAsSeq(chunk.take(n))
+
+  override def takeRight(n: Int): Seq[O] =
+    new ChunkAsSeq(chunk.takeRight(n))
+
+  override def to[C1](factory: Factory[O, C1]): C1 =
+    chunk.to(factory)
+
+  override def toArray[B >: O: ClassTag]: Array[B] =
+    chunk.toArray
+
+  override def toList: List[O] =
+    chunk.toList
+
+  override def toVector: Vector[O] =
+    chunk.toVector
+
+  override def toString: String =
+    chunk.toString
+
+  override def zipWithIndex: Seq[(O, Int)] =
+    new ChunkAsSeq(chunk.zipWithIndex)
 }
 
 private[fs2] trait ChunkCompanion213And3Compat { self: Chunk.type =>
 
   protected def platformIterable[O](i: Iterable[O]): Option[Chunk[O]] =
     i match {
-      case a: immutable.ArraySeq[O] => Some(arraySeq(a))
-      case _                        => None
+      case a: ArraySeq[O] => Some(arraySeq(a))
+      case _              => None
     }
 
   /** Creates a chunk backed by an immutable `ArraySeq`. */
-  def arraySeq[O](arraySeq: immutable.ArraySeq[O]): Chunk[O] = {
+  def arraySeq[O](arraySeq: ArraySeq[O]): Chunk[O] = {
     val arr = arraySeq.unsafeArray.asInstanceOf[Array[O]]
     array(arr)(ClassTag[O](arr.getClass.getComponentType))
   }
 
   /** Creates a chunk from a `scala.collection.IterableOnce`. */
-  def iterableOnce[O](i: collection.IterableOnce[O]): Chunk[O] =
+  def iterableOnce[O](i: IterableOnce[O]): Chunk[O] =
     iterator(i.iterator)
 }
