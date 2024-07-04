37968c1171c90502e2096d782d511ab564772936
Michael Pilquist <mpilquist@gmail.com>
2024-07-04 12:57:17 -0400

    Scaladocs

--- core/shared/src/main/scala/fs2/hashing/Hash.scala
+++ core/shared/src/main/scala/fs2/hashing/Hash.scala
@@ -24,25 +24,53 @@ package hashing
 
 import cats.effect.Sync
 
+/** Mutable data structure that incrementally computes a hash from chunks of bytes.
+  *
+  * To compute a hash, call `addChunk` one or more times and then call `computeAndReset`.
+  * The result of `computeAndReset` is the hash value of all the bytes since the last call
+  * to `computeAndReset`.
+  *
+  * A `Hash` does **not** store all bytes between calls to `computeAndReset` and hence is safe
+  * for computing hashes over very large data sets using constant memory.
+  *
+  * A `Hash` may be called from different fibers but operations on a hash should not be called
+  * concurrently.
+  */
 trait Hash[F[_]] {
+
+  /** Adds the specified bytes to the current hash computation.
+    */
   def addChunk(bytes: Chunk[Byte]): F[Unit]
+
+  /** Finalizes the hash computation, returns the result, and resets this hash for a fresh computation.
+    */
   def computeAndReset: F[Chunk[Byte]]
 
   protected def unsafeAddChunk(slice: Chunk.ArraySlice[Byte]): Unit
   protected def unsafeComputeAndReset(): Chunk[Byte]
 
+  /** Returns a pipe that updates this hash computation with chunks of bytes pulled from the pipe.
+    */
   def update: Pipe[F, Byte, Byte] =
     _.mapChunks { c =>
       unsafeAddChunk(c.toArraySlice)
       c
     }
 
+  /** Returns a stream that when pulled, pulls on the source, updates this hash with bytes emitted,
+    * and sends those bytes to the supplied sink. Upon termination of the source and sink, the hash is emitted.
+    */
   def observe(source: Stream[F, Byte], sink: Pipe[F, Byte, Nothing]): Stream[F, Byte] =
     update(source).through(sink) ++ Stream.evalUnChunk(computeAndReset)
 
+  /** Pipe that outputs the hash of the source after termination of the source.
+    */
   def hash: Pipe[F, Byte, Byte] =
     source => observe(source, _.drain)
 
+  /** Pipe that, at termination of the source, verifies the hash of seen bytes matches the expected value
+    * or otherwise fails with a [[HashVerificationException]].
+    */
   def verify(expected: Chunk[Byte])(implicit F: RaiseThrowable[F]): Pipe[F, Byte, Byte] =
     source =>
       update(source)
--- core/shared/src/main/scala/fs2/hashing/Hashing.scala
+++ core/shared/src/main/scala/fs2/hashing/Hashing.scala
@@ -28,16 +28,35 @@ import cats.effect.Sync
   *
   * The [[create]] method returns an action that instantiates a fresh `Hash` object.
   * `Hash` is a mutable object that supports incremental computation of hashes. A `Hash`
-  * instance should be created for each hash you want to compute.
+  * instance should be created for each hash you want to compute (`Hash` objects may be
+  * reused to compute multiple hashes but care must be taken to ensure no concurrent usage).
   */
 trait Hashing[F[_]] {
+
+  /** Creates a new hash using the specified hashing algorithm. */
   def create(algorithm: String): F[Hash[F]]
+
+  /** Creates a new MD-5 hash. */
   def md5: F[Hash[F]] = create("MD-5")
+
+  /** Creates a new SHA-1 hash. */
   def sha1: F[Hash[F]] = create("SHA-1")
+
+  /** Creates a new SHA-256 hash. */
   def sha256: F[Hash[F]] = create("SHA-256")
+
+  /** Creates a new SHA-384 hash. */
   def sha384: F[Hash[F]] = create("SHA-384")
+
+  /** Creates a new SHA-512 hash. */
   def sha512: F[Hash[F]] = create("SHA-512")
 
+  /** Returns a pipe that hashes the source byte stream and outputs the hash.
+    *
+    * For more sophisticated use cases, such as writing the contents of a stream
+    * to a file while simultaneously computing a hash, use `create` or `sha256` or
+    * similar to create a `Hash[F]`.
+    */
   def hashWith(hash: F[Hash[F]]): Pipe[F, Byte, Byte] =
     source => Stream.eval(hash).flatMap(h => h.hash(source))
 }
