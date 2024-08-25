c8acb79719b7448d2d5a6da9324844c68dd7fdf9
Michael Pilquist <mpilquist@gmail.com>
2024-08-25 11:15:57 -0400

    Fix build

--- core/native/src/main/scala/fs2/hash.scala
+++ core/native/src/main/scala/fs2/hash.scala
@@ -50,6 +50,6 @@ object hash {
       algorithm: HashAlgorithm
   )(implicit F: Sync[F]): Pipe[F, Byte, Byte] = {
     val h = Hashing.forSync[F]
-    s => h.hashWith(h.hash(algorithm))(s).map(_.bytes).unchunks
+    s => h.hashWith(h.hasher(algorithm))(s).map(_.bytes).unchunks
   }
 }
