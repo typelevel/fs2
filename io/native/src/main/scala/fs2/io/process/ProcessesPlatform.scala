eaebb468cccb1507917b58f26f73f9a8bbe90500
rahulrangers <rahulreddypurmani123@gmail.com>
2025-08-08 13:11:22 +0530

    remove implicit z

--- io/native/src/main/scala/fs2/io/process/ProcessesPlatform.scala
+++ io/native/src/main/scala/fs2/io/process/ProcessesPlatform.scala
@@ -95,7 +95,7 @@ private[process] trait ProcessesCompanionPlatform extends ProcessesCompanionJvmN
               stdoutPipe: (Int, Int),
               stderrPipe: (Int, Int)
           ): F[NativeProcess] = F.blocking {
-            Zone.apply[NativeProcess] { implicit z =>
+            Zone {
               val envMap =
                 if (process.inheritEnv)
                   sys.env ++ process.extraEnv
