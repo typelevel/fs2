baad3dec1c118529131f0d998e8ac3143dd80f3a
rahulrangers <rahulreddypurmani123@gmail.com>
2025-08-19 18:55:54 +0530

    format

--- io/jvm/src/main/scala/fs2/io/net/SelectingDatagramSocket.scala
+++ io/jvm/src/main/scala/fs2/io/net/SelectingDatagramSocket.scala
@@ -33,6 +33,7 @@ import java.nio.ByteBuffer
 import java.nio.channels.{DatagramChannel, SelectionKey}
 import java.net.{NetworkInterface => JNetworkInterface}
 import com.comcast.ip4s.MulticastJoin
+import scala.jdk.CollectionConverters._
 
 private final class SelectingDatagramSocket[F[_]: LiftIO] private (
     selector: Selector,
